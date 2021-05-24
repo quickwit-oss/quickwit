5094053f91b894a2be3bf3fd1c297974fe87efd7
Evance Soumaoro <evanxg852000@gmail.com>
2021-05-24 08:39:17 +0000

    fixed default help when no command is specified

---
+++ quickwit-hot-directory/src/lib.rs
@@ -0,0 +1,41 @@
+/*
+    Quickwit
+    Copyright (C) 2021 Quickwit Inc.
+
+    Quickwit is offered under the AGPL v3.0 and as commercial software.
+    For commercial licensing, contact us at hello@quickwit.io.
+
+    AGPL:
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*!
+This crate contains all of the building pieces that make quickwit's IO possible.
+
+- The `StorageDirectory` justs wraps a `Storage` trait to make it compatible with tantivy's Directory API.
+- The `HotDirectory` wraps another directory with a static cache.
+- The `CachingDirectory` wraps a Directory with a dynamic cache.
+- The `DebugDirectory` acts as a proxy to another directory to instrument it and record all of its IO.
+*/
+#![allow(missing_docs)]
+
+mod caching_directory;
+mod debug_proxy_directory;
+mod hot_directory;
+mod storage_directory;
+
+pub use self::caching_directory::{Cache, CachingDirectory};
+pub use self::debug_proxy_directory::{DebugProxyDirectory, ReadOperation};
+pub use self::hot_directory::{write_hotcache, HotDirectory};
+pub use self::storage_directory::StorageDirectory;
