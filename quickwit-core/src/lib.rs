484d97e150f72d135c3ab744f120b4394977f436
Paul Masurel <paul@quickwit.io>
2021-05-25 15:09:55 +0900

    Added lint forbidding missing docs in quickwit-core

--- quickwit-core/src/lib.rs
+++ quickwit-core/src/lib.rs
@@ -19,5 +19,6 @@
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+#![warn(missing_docs)]
 
 pub mod index;
