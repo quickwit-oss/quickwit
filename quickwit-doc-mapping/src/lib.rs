64467f8b1602189e38992e78102ecd20f06b5581
Paul Masurel <paul@quickwit.io>
2021-05-25 15:07:34 +0900

    Missing docs docmapping.

--- quickwit-doc-mapping/src/lib.rs
+++ quickwit-doc-mapping/src/lib.rs
@@ -19,6 +19,7 @@
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+#![warn(missing_docs)]
 
 // This is just a placeholder implementation to get going, @fmassot, please take over ;)
 pub enum DocMapping {
