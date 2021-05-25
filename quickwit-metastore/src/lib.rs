412e3f0bca6dde4fdd00a46c6d34af66b88ef73b
Paul Masurel <paul@quickwit.io>
2021-05-25 15:05:21 +0900

    Added lint for missing doc in metastore

--- quickwit-metastore/src/lib.rs
+++ quickwit-metastore/src/lib.rs
@@ -19,6 +19,7 @@
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+#![warn(missing_docs)]
 
 mod error;
 mod metastore;
