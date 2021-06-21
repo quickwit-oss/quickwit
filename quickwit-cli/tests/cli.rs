40ea89bbf0ee3c21d634803b768103d621a9ad13
Evance Soumaoro <evanxg852000@gmail.com>
2021-06-21 11:10:54 +0000

    refactored added tests

--- quickwit-cli/tests/cli.rs
+++ quickwit-cli/tests/cli.rs
@@ -1,62 +1,201 @@
+/*
+    Quickwit
+    Copyright (C) 2021 Quickwit Inc.
+
+    Quickwit is offered under the AGPL v3.0 and as commercial software.
+    For commercial licensing, contact us at hello@quickwit.io.
+
+    AGPL:
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#![allow(clippy::bool_assert_comparison)]
+
+mod helpers;
+
+use crate::helpers::{create_test_env, make_command};
 use anyhow::Result;
-use assert_cmd::prelude::*;
+use helpers::TestEnv;
 use predicates::prelude::*;
-use std::process::Command;
+use std::path::Path;
 
-/// Creates a quickwit-cli command with provided list of arguments.
-fn make_command(argument: &str) -> Command {
-    let mut cmd = Command::cargo_bin("quickwit-cli").unwrap();
-    for arg in argument.split_whitespace() {
-        cmd.arg(arg);
-    }
-    cmd
-}
-
-#[test]
-fn test_cmd_help() -> anyhow::Result<()> {
-    let mut cmd = make_command("--help");
-
-    cmd.assert()
-        .success()
-        .stdout(predicate::str::contains("USAGE"));
-
-    Ok(())
-}
-
-#[test]
-fn test_cmd_create_index() -> Result<()> {
-    let test_dir = create_directory();
-
-    make_command("new")
-        .assert()
-        .failure()
-        .stderr(predicate::str::contains("--index-uri <INDEX URI>"));
-
-    let uri = format!("file://{}/indices/data", test_dir.path().display());
+fn create_wikipedia_index(test_env: &TestEnv) {
     make_command(
         format!(
-            "new --index-uri {} --no-timestamp-field --doc-mapper-type wikipedia",
-            uri
+            "new --index-uri {} --doc-mapper-type wikipedia",
+            test_env.uri
         )
         .as_str(),
     )
     .assert()
     .success();
+    assert!(test_env.path.join("quickwit.json").exists());
+}
+
+fn create_logs_index(test_env: &TestEnv) {
+    make_command(
+        format!(
+            "new --index-uri {} --doc-mapper-config-path {}",
+            test_env.uri,
+            test_env.config_path.display()
+        )
+        .as_str(),
+    )
+    .assert()
+    .success();
+    assert!(test_env.path.join("quickwit.json").exists());
+}
+
+fn index_data(uri: &str, input_path: &Path) {
+    make_command(
+        format!(
+            "index --index-uri {} --input-path {}",
+            uri,
+            input_path.display(),
+        )
+        .as_str(),
+    )
+    .assert()
+    .success()
+    .stdout(predicate::str::contains("Indexed"))
+    .stdout(predicate::str::contains("documents in"))
+    .stdout(predicate::str::contains(
+        "You can now query your index with",
+    ));
+}
+
+fn delete_index(test_env: &TestEnv) {
+    make_command(format!("delete --index-uri {} ", test_env.uri).as_str())
+        .assert()
+        .success();
+    assert_eq!(test_env.path.exists(), false);
+}
+
+#[test]
+fn test_cmd_help() -> anyhow::Result<()> {
+    let mut cmd = make_command("--help");
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("USAGE"));
+    Ok(())
+}
+
+#[test]
+fn test_cmd_new() -> Result<()> {
+    let test_env = create_test_env()?;
+    create_wikipedia_index(&test_env);
+
+    // Attempt to create with ill-formed new command.
+    make_command("new")
+        .assert()
+        .failure()
+        .stderr(predicate::str::contains("--index-uri <INDEX URI>"));
 
     Ok(())
 }
 
-/*
+#[test]
+fn test_cmd_new_on_existing_index() -> Result<()> {
+    let test_env = create_test_env()?;
+    create_wikipedia_index(&test_env);
+
     make_command(
-        "new --index-uri file://quickwit-dev/evance/data --no-timestamp-field --doc-mapper-type wikipedia",
+        format!(
+            "new --index-uri {} --doc-mapper-type wikipedia",
+            test_env.uri
+        )
+        .as_str(),
     )
     .assert()
-    .success();
+    .failure()
+    .stderr(predicate::str::contains("already exists"));
 
+    Ok(())
+}
 
-    let mut cmd = make_command(vec!["new"]);
-    cmd.assert()
+#[test]
+fn test_cmd_index() -> Result<()> {
+    let test_env = create_test_env()?;
+    create_logs_index(&test_env);
+
+    // Indexing data on non-existing index.
+    make_command(
+        format!(
+            "index --index-uri {}/non-existing-index --input-path {}",
+            test_env.uri,
+            test_env.log_data_path.display(),
+        )
+        .as_str(),
+    )
+    .assert()
+    .failure()
+    .stderr(predicate::str::contains("does not exist"));
+
+    // Index on non-existing data file.
+    make_command(
+        format!(
+            "index --index-uri {} --input-path {}",
+            test_env.uri,
+            test_env.dir.path().join("non-existing-data.json").display(),
+        )
+        .as_str(),
+    )
+    .assert()
+    .failure()
+    .stderr(predicate::str::contains("No such file or directory"));
+
+    index_data(&test_env.uri, test_env.log_data_path.as_path());
+
+    // Indexing data with piped input.
+    make_command(format!("index --index-uri {} ", test_env.uri).as_str())
+        .pipe_stdin(test_env.log_data_path)?
+        .assert()
         .success()
-        .stdout(predicate::str::contains("USAGE"));
+        .stdout(predicate::str::contains("Indexed"))
+        .stdout(predicate::str::contains("documents in"))
+        .stdout(predicate::str::contains(
+            "You can now query your index with",
+        ));
 
-*/
+    Ok(())
+}
+
+#[test]
+fn test_cmd_delete() -> Result<()> {
+    let test_env = create_test_env()?;
+    create_wikipedia_index(&test_env);
+
+    // Delete empty index.
+    make_command(format!("delete --index-uri {} --dry-run", test_env.uri).as_str())
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("Only the index will be deleted"));
+
+    // Index some data.
+    index_data(&test_env.uri, test_env.wiki_data_path.as_path());
+
+    // Delete index with dry-run.
+    make_command(format!("delete --index-uri {} --dry-run", test_env.uri).as_str())
+        .assert()
+        .success()
+        .stdout(predicate::str::contains(
+            "The following files will be removed",
+        ))
+        .stdout(predicate::str::contains("/hotcache"))
+        .stdout(predicate::str::contains("/.manifest"));
+
+    delete_index(&test_env);
+
+    Ok(())
+}
--- quickwit-cli/tests/helpers.rs
+++ quickwit-cli/tests/helpers.rs
@@ -1,19 +1,117 @@
+/*
+    Quickwit
+    Copyright (C) 2021 Quickwit Inc.
 
-pub fn create_directory() {
-    let path = temp_dir();
-    create_dir(path.clone()).unwrap();
-    path
+    Quickwit is offered under the AGPL v3.0 and as commercial software.
+    For commercial licensing, contact us at hello@quickwit.io.
+
+    AGPL:
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+use assert_cmd::Command;
+use std::fs::OpenOptions;
+use std::io::Write;
+use std::path::Path;
+use std::path::PathBuf;
+use tempfile::tempdir;
+use tempfile::TempDir;
+
+const DEFAULT_DOC_MAPPER: &str = r#"{
+    "store_source": true,
+    "default_search_fields": ["event"],
+    "timestamp_field": "ts",
+    "field_mappings": [
+        {
+            "name": "event",
+            "type": "text"
+        },
+        {
+            "name": "level",
+            "type": "text"
+        },
+        {
+            "name": "ts",
+            "type": "i64"
+        }
+    ]
+}"#;
+
+const LOGS_JSON_DOCS: &str = r#"{"event": "foo", "level": "info", "ts": 2}
+{"event": "bar", "level": "error", "ts": 3}
+{"event": "baz", "level": "warning", "ts": 9}
+{"event": "buz", "level": "debug", "ts": 12}
+{"event": "biz", "level": "info", "ts": 13}"#;
+
+const WIKI_JSON_DOCS: &str = r#"{"body": "foo", "title": "shimray", "url": "https://wiki.com?id=10"}
+{"body": "bar", "title": "shimray", "url": "https://wiki.com?id=12"}
+{"body": "baz", "title": "preshow", "url": "https://wiki.com?id=11"}
+{"body": "buz", "title": "frederick", "url": "https://wiki.com?id=48"}
+{"body": "biz", "title": "modern", "url": "https://wiki.com?id=13"}
+"#;
+
+/// Creates a file with a content
+pub fn create_file(path: &Path, data: &str) {
+    let mut file = OpenOptions::new()
+        .create(true)
+        .write(true)
+        .truncate(true)
+        .open(path)
+        .unwrap();
+
+    file.write_all(data.as_bytes()).unwrap();
+    file.flush().unwrap();
+    file.sync_all().unwrap();
 }
 
-pub fn create_file() {
-    let path = temp_dir();
-    create_dir(path.clone()).unwrap();
-    path
+/// Creates a quickwit-cli command with provided list of arguments.
+pub fn make_command(argument: &str) -> Command {
+    let mut cmd = Command::cargo_bin("quickwit-cli").unwrap();
+    for arg in argument.split_whitespace() {
+        cmd.arg(arg);
+    }
+    cmd
 }
 
-/// Creates a file with given content
-pub fn create_file(config: &str) -> PathBuf {
-    let path = temp_file();
-    overwrite_file(path.clone(), config);
-    path
+/// A struct to hold few info about the test environement.
+pub struct TestEnv {
+    pub dir: TempDir,
+    pub path: PathBuf,
+    pub uri: String,
+    pub config_path: PathBuf,
+    pub log_data_path: PathBuf,
+    pub wiki_data_path: PathBuf,
+}
+
+/// Creates all necessary artifacts in a test environement.
+pub fn create_test_env() -> anyhow::Result<TestEnv> {
+    let dir = tempdir()?;
+    let path = PathBuf::from(format!("{}/indices/data", dir.path().display()));
+    let uri = format!("file://{}", path.display());
+    let config_path = dir.path().join("config.json");
+    create_file(&config_path, DEFAULT_DOC_MAPPER);
+    let log_data_path = dir.path().join("logs.json");
+    create_file(&log_data_path, LOGS_JSON_DOCS);
+    let wiki_data_path = dir.path().join("wikis.json");
+    create_file(&wiki_data_path, WIKI_JSON_DOCS);
+
+    Ok(TestEnv {
+        dir,
+        path,
+        uri,
+        config_path,
+        log_data_path,
+        wiki_data_path,
+    })
 }
