87acdf3d2710b0d6e0bd4f4a5f6a7d1eb74b407b
Minoru Osuka <minoru.osuka@gmail.com>
2021-08-14 21:50:38 +0900

    Add cluster endpoint.

---
+++ quickwit-serve/src/grpc_adapter/search_adapter.rs
@@ -0,0 +1,97 @@
+/*
+ * Copyright (C) 2021 Quickwit Inc.
+ *
+ * Quickwit is offered under the AGPL v3.0 and as commercial software.
+ * For commercial licensing, contact us at hello@quickwit.io.
+ *
+ * AGPL:
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+use std::sync::Arc;
+
+use async_trait::async_trait;
+use tokio_stream::wrappers::UnboundedReceiverStream;
+
+use quickwit_proto::{
+    search_service_server as grpc, LeafSearchStreamRequest, LeafSearchStreamResult,
+};
+use quickwit_search::{SearchError, SearchService, SearchServiceImpl};
+
+/// gRPC adapter that wraped SearchService.
+#[derive(Clone)]
+pub struct GrpcSearchAdapter(Arc<dyn SearchService>);
+
+impl From<Arc<SearchServiceImpl>> for GrpcSearchAdapter {
+    fn from(search_service_arc: Arc<SearchServiceImpl>) -> Self {
+        GrpcSearchAdapter(search_service_arc)
+    }
+}
+
+#[async_trait]
+impl grpc::SearchService for GrpcSearchAdapter {
+    async fn root_search(
+        &self,
+        request: tonic::Request<quickwit_proto::SearchRequest>,
+    ) -> Result<tonic::Response<quickwit_proto::SearchResult>, tonic::Status> {
+        let search_request = request.into_inner();
+        let search_result = self
+            .0
+            .root_search(search_request)
+            .await
+            .map_err(SearchError::convert_to_tonic_status)?;
+        Ok(tonic::Response::new(search_result))
+    }
+
+    async fn leaf_search(
+        &self,
+        request: tonic::Request<quickwit_proto::LeafSearchRequest>,
+    ) -> Result<tonic::Response<quickwit_proto::LeafSearchResult>, tonic::Status> {
+        let leaf_search_request = request.into_inner();
+        let leaf_search_result = self
+            .0
+            .leaf_search(leaf_search_request)
+            .await
+            .map_err(SearchError::convert_to_tonic_status)?;
+        Ok(tonic::Response::new(leaf_search_result))
+    }
+
+    async fn fetch_docs(
+        &self,
+        request: tonic::Request<quickwit_proto::FetchDocsRequest>,
+    ) -> Result<tonic::Response<quickwit_proto::FetchDocsResult>, tonic::Status> {
+        let fetch_docs_request = request.into_inner();
+        let fetch_docs_result = self
+            .0
+            .fetch_docs(fetch_docs_request)
+            .await
+            .map_err(SearchError::convert_to_tonic_status)?;
+        Ok(tonic::Response::new(fetch_docs_result))
+    }
+
+    type LeafSearchStreamStream =
+        UnboundedReceiverStream<Result<LeafSearchStreamResult, tonic::Status>>;
+    async fn leaf_search_stream(
+        &self,
+        request: tonic::Request<LeafSearchStreamRequest>,
+    ) -> Result<tonic::Response<Self::LeafSearchStreamStream>, tonic::Status> {
+        let leaf_search_request = request.into_inner();
+        let leaf_search_result = self
+            .0
+            .leaf_search_stream(leaf_search_request)
+            .await
+            .map_err(SearchError::convert_to_tonic_status)?;
+        Ok(tonic::Response::new(leaf_search_result))
+    }
+}
---
+++ quickwit-serve/src/http_handler/cluster.rs
@@ -0,0 +1,60 @@
+use std::convert::Infallible;
+use std::sync::Arc;
+
+use warp::hyper::header::CONTENT_TYPE;
+use warp::hyper::StatusCode;
+use warp::reply;
+use warp::Filter;
+use warp::Rejection;
+use warp::Reply;
+
+use quickwit_cluster::service::ClusterService;
+
+use crate::ApiError;
+
+/// cluster handler.
+pub fn cluster_handler<TClusterService: ClusterService>(
+    cluster_service: Arc<TClusterService>,
+) -> impl Filter<Extract = impl warp::Reply, Error = Rejection> + Clone {
+    members_filter()
+        .and(warp::any().map(move || cluster_service.clone()))
+        .and_then(members)
+}
+
+fn members_filter() -> impl Filter<Extract = (), Error = Rejection> + Clone {
+    warp::path!("cluster" / "members").and(warp::get())
+}
+
+async fn members<TClusterService: ClusterService>(
+    cluster_service: Arc<TClusterService>,
+) -> Result<impl warp::Reply, Infallible> {
+    Ok(make_reply(members_endpoint(&*cluster_service).await))
+}
+
+async fn members_endpoint<TClusterService: ClusterService>(
+    cluster_service: &TClusterService,
+) -> Result<quickwit_proto::MembersResult, ApiError> {
+    let members_request = quickwit_proto::MembersRequest {};
+    let members_result = cluster_service.members(members_request).await?;
+    Ok(members_result)
+}
+
+fn make_reply<T: serde::Serialize>(result: Result<T, ApiError>) -> impl Reply {
+    let status_code: StatusCode;
+    let body_json = match result {
+        Ok(success) => {
+            status_code = StatusCode::OK;
+            serde_json::to_string(&success)
+        }
+        Err(err) => {
+            status_code = err.http_status_code();
+            serde_json::to_string(&err)
+        }
+    }
+    .unwrap_or_else(|_| {
+        tracing::error!("Error: the response serialization failed.");
+        "Error: Failed to serialize response.".to_string()
+    });
+    let reply_with_header = reply::with_header(body_json, CONTENT_TYPE, "application/json");
+    reply::with_status(reply_with_header, status_code)
+}
