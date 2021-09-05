78af612d9ca996a4c0595b22a21a8f6083f18b1e
Adrien Guillo <adrien.guillo@gmail.com>
2021-09-05 10:21:34 -0700

    Refactor cluster tests

--- quickwit-cluster/src/cluster.rs
+++ quickwit-cluster/src/cluster.rs
@@ -18,7 +18,7 @@
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 use std::fs;
-use std::net::SocketAddr;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use std::path::Path;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
@@ -29,7 +29,9 @@ use quickwit_swim::prelude::{
     Cluster as ArtilleryCluster, ClusterConfig as ArtilleryClusterConfig,
 };
 use tokio::sync::watch;
+use tokio::time::timeout;
 use tokio_stream::wrappers::WatchStream;
+use tokio_stream::StreamExt;
 use tracing::{debug, error, info, warn};
 use uuid::Uuid;
 
@@ -43,7 +45,7 @@ const CLUSTER_EVENT_TIMEOUT: Duration = Duration::from_millis(200);
 /// Reads the key that makes a node unique from the given file.
 /// If the file does not exist, it generates an ID and writes it to the file
 /// so that it can be reused on reboot.
-pub fn read_host_key(host_key_path: &Path) -> ClusterResult<Uuid> {
+pub fn read_or_create_host_key(host_key_path: &Path) -> ClusterResult<Uuid> {
     let host_key;
 
     if host_key_path.exists() {
@@ -205,15 +207,35 @@ impl Cluster {
 
     /// Specify the address of a running node and join the cluster to which the node belongs.
     pub async fn add_peer_node(&self, peer_addr: SocketAddr) {
-        info!(peer_addr=?peer_addr, "Add peer node.");
+        info!(self_addr = ?self.listen_addr, peer_addr = ?peer_addr, "Adding peer node.");
         self.artillery_cluster.add_seed_node(peer_addr);
     }
 
     /// Leave the cluster.
     pub async fn leave(&self) {
-        info!("Leave the cluster.");
+        info!(self_addr = ?self.listen_addr, "Leaving the cluster.");
         self.artillery_cluster.leave_cluster();
         self.stop.store(true, Ordering::Relaxed);
+    }
+
+    /// Convenience method for testing that waits for the predicate to hold true for the cluster's
+    /// members.
+    pub async fn wait_for_members<F>(
+        self: &Cluster,
+        mut predicate: F,
+        timeout_after: Duration,
+    ) -> anyhow::Result<()>
+    where
+        F: FnMut(&Vec<Member>) -> bool,
+    {
+        timeout(
+            timeout_after,
+            self.member_change_watcher()
+                .skip_while(|members| !predicate(members))
+                .next(),
+        )
+        .await?;
+        Ok(())
     }
 }
 
@@ -256,16 +278,25 @@ fn log_artillery_event(artillery_member_event: ArtilleryMemberEvent) {
     };
 }
 
+/// Creates a local cluster listening on a random port.
+pub fn create_cluster_for_test() -> anyhow::Result<Cluster> {
+    let peer_uuid = Uuid::new_v4();
+    let port = quickwit_common::net::find_available_port()?;
+    let peer_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), port);
+    let cluster = Cluster::new(peer_uuid, peer_addr)?;
+    Ok(cluster)
+}
+
 #[cfg(test)]
 mod tests {
     use std::net::SocketAddr;
     use std::time::Duration;
-    use std::{thread, time};
 
+    use itertools::Itertools;
     use quickwit_swim::prelude::{ArtilleryMember, ArtilleryMemberState};
 
-    use crate::cluster::{convert_member, read_host_key, Member};
-    use crate::test_utils::{available_port, test_cluster};
+    use super::*;
+    use crate::cluster::{convert_member, read_or_create_host_key, Member};
 
     #[tokio::test]
     async fn test_cluster_read_host_key() {
@@ -273,134 +304,99 @@ mod tests {
         let host_key_path = tmp_dir.path().join("host_key");
 
         // Since the directory does not exist, generate UUID on the specified host_key_path.
-        let host_key1 = read_host_key(host_key_path.as_path()).unwrap();
-        println!("host_key1={:?}", host_key1);
+        let host_key1 = read_or_create_host_key(host_key_path.as_path()).unwrap();
 
         // Read the generated UUID.
-        let host_key2 = read_host_key(host_key_path.as_path()).unwrap();
-        println!("host_key2={:?}", host_key2);
+        let host_key2 = read_or_create_host_key(host_key_path.as_path()).unwrap();
 
         assert_eq!(host_key1, host_key2);
-
-        tmp_dir.close().unwrap();
     }
 
     #[tokio::test]
     async fn test_cluster_convert_member() {
         let tmp_dir = tempfile::tempdir().unwrap();
         let host_key_path = tmp_dir.path().join("host_key");
-        let host_key = read_host_key(host_key_path.as_path()).unwrap();
-        println!("host_key={:?}", host_key);
-
-        let tmp_port = available_port().unwrap();
-        let addr_string = format!("127.0.0.1:{}", tmp_port);
-        let remote_host: SocketAddr = addr_string.as_str().parse().unwrap();
-        println!("remote_host={:?}", remote_host);
-
+        let host_key = read_or_create_host_key(host_key_path.as_path()).unwrap();
+        let remote_host = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 0);
         {
             let artillery_member =
                 ArtilleryMember::new(host_key, remote_host, 0, ArtilleryMemberState::Alive);
-            println!("artillery_member={:?}", artillery_member);
 
             let member = convert_member(artillery_member, remote_host);
-            println!("member={:?}", member);
-
-            let expected = Member {
+            let expected_member = Member {
                 host_key,
                 listen_addr: remote_host,
                 is_self: false,
             };
-
-            assert_eq!(member, expected);
+            assert_eq!(member, expected_member);
         }
-
         {
             let artillery_member = ArtilleryMember::current(host_key);
-            println!("artillery_member={:?}", artillery_member);
-
             let member = convert_member(artillery_member, remote_host);
-            println!("member={:?}", member);
-
-            let expected = Member {
+            let expected_member = Member {
                 host_key,
                 listen_addr: remote_host,
                 is_self: true,
             };
-
-            assert_eq!(member, expected);
+            assert_eq!(member, expected_member);
         }
-
-        tmp_dir.close().unwrap();
     }
 
     #[tokio::test]
     async fn test_cluster_single_node() -> anyhow::Result<()> {
-        let tmp_dir = tempfile::tempdir()?;
-
-        let cluster = test_cluster(tmp_dir.path().join("host_key").as_path())?;
+        let cluster = create_cluster_for_test()?;
 
         let members: Vec<SocketAddr> = cluster
             .members()
             .iter()
             .map(|member| member.listen_addr)
             .collect();
-        println!("member={:?}", members);
-        let expected = vec![cluster.listen_addr];
-        assert_eq!(members, expected);
+        let expected_members = vec![cluster.listen_addr];
+        assert_eq!(members, expected_members);
 
         cluster.leave().await;
-
-        tmp_dir.close()?;
-
         Ok(())
     }
 
     #[tokio::test]
-    async fn test_cluster_multiple_node() -> anyhow::Result<()> {
-        let tmp_dir = tempfile::tempdir()?;
+    async fn test_cluster_multiple_nodes() -> anyhow::Result<()> {
+        let cluster1 = create_cluster_for_test()?;
+        let cluster2 = create_cluster_for_test()?;
+        let cluster3 = create_cluster_for_test()?;
 
-        let cluster1 = test_cluster(tmp_dir.path().join("host_key1").as_path())?;
-
-        let cluster2 = test_cluster(tmp_dir.path().join("host_key2").as_path())?;
         cluster2.add_peer_node(cluster1.listen_addr).await;
-
-        let cluster3 = test_cluster(tmp_dir.path().join("host_key3").as_path())?;
         cluster3.add_peer_node(cluster1.listen_addr).await;
 
-        // Wait for the cluster to be configured.
-        tokio::time::sleep(Duration::from_secs(5)).await;
+        let ten_secs = Duration::from_secs(10);
 
-        dbg!("hello");
-        let mut members: Vec<SocketAddr> = cluster1
+        for cluster in [&cluster1, &cluster2, &cluster3] {
+            cluster
+                .wait_for_members(|members| members.len() == 3, ten_secs)
+                .await?;
+        }
+        let members: Vec<SocketAddr> = cluster1
             .members()
             .iter()
             .map(|member| member.listen_addr)
+            .sorted()
             .collect();
-        members.sort();
-        println!("{:?}", members);
-        let mut expected = vec![
+        let mut expected_members = vec![
             cluster1.listen_addr,
             cluster2.listen_addr,
             cluster3.listen_addr,
         ];
-        expected.sort();
-        println!("{:?}", expected);
-        assert_eq!(members, expected);
+        expected_members.sort();
+        assert_eq!(members, expected_members);
 
-        cluster1.leave().await;
-
-        // Wait for leaving the cluster.
-        thread::sleep(time::Duration::from_millis(10));
-
-        cluster2.leave().await;
-
-        // Wait for leaving the cluster.
-        thread::sleep(time::Duration::from_millis(10));
+        drop(cluster2);
+        cluster1
+            .wait_for_members(|members| members.len() == 2, ten_secs)
+            .await?;
 
         cluster3.leave().await;
-
-        tmp_dir.close().unwrap();
-
+        cluster1
+            .wait_for_members(|members| members.len() == 1, ten_secs)
+            .await?;
         Ok(())
     }
 }
