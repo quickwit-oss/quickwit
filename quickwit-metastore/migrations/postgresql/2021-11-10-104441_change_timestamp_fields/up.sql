ab03e8fc5d58eca073ed6c6b6232b21f7fedd1e8
Evance Soumaoro <evanxg852000@gmail.com>
2021-11-11 10:27:17 +0000

    removed nullable on timestamp and using UTC for created_at, updated_at fields

--- quickwit-metastore/migrations/postgresql/2021-11-10-104441_change_timestamp_fields/up.sql
+++ quickwit-metastore/migrations/postgresql/2021-11-10-104441_change_timestamp_fields/up.sql
@@ -4,9 +4,22 @@ DROP COLUMN update_timestamp;
 
 -- create new timestamp fields
 ALTER TABLE splits
-ADD created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
+ADD created_at TIMESTAMP NOT NULL DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC');
 ALTER TABLE splits
-ADD updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
+ADD updated_at TIMESTAMP NOT NULL DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC');
+
+-- replace diesel helper function to make sure it uses UTC timestamp
+CREATE OR REPLACE FUNCTION diesel_set_updated_at() RETURNS trigger AS $$
+BEGIN
+    IF (
+        NEW IS DISTINCT FROM OLD AND
+        NEW.updated_at IS NOT DISTINCT FROM OLD.updated_at
+    ) THEN
+        NEW.updated_at := (CURRENT_TIMESTAMP AT TIME ZONE 'UTC');
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
 
 -- apply update trigger
 SELECT diesel_manage_updated_at('splits');
