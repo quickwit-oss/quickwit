c83f9d7652e4ab6bcc2f88cc179a42fe70886935
PSeitz <PSeitz@users.noreply.github.com>
2021-12-20 11:42:56 +0100

    Add query param for sorting search results (#917)
    Fixes #904

--- quickwit-index-config/src/sort_by.rs
+++ quickwit-index-config/src/sort_by.rs
@@ -17,6 +17,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+use quickwit_proto::SearchRequest;
 use serde::{Deserialize, Serialize};
 use tantivy::Order as TantivyOrder;
 
@@ -25,7 +26,7 @@ use tantivy::Order as TantivyOrder;
 
 /// Sort order, either ascending or descending.
 /// Use `SortOrder::Desc` for top-k queries.
-#[derive(Clone, Copy, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(rename_all = "lowercase")]
 pub enum SortOrder {
     /// Ascending order.
@@ -52,7 +53,31 @@ impl From<SortOrder> for TantivyOrder {
 /// Specifies how documents are sorted.
 /// In case of a tie, the documents are ordered according to descending `(split_id, segment_ord,
 /// doc_id)`.
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct SortByField {
+    /// Name of the field to sort by.
+    pub field_name: String,
+    /// Order to sort by. A usual top-k search implies a descending order.
+    pub order: SortOrder,
+}
+
+impl From<String> for SortByField {
+    fn from(string: String) -> Self {
+        let (field_name, order) = if let Some(rest) = string.strip_prefix('+') {
+            (rest.trim().to_string(), SortOrder::Asc)
+        } else if let Some(rest) = string.strip_prefix('-') {
+            (rest.trim().to_string(), SortOrder::Desc)
+        } else {
+            (string.trim().to_string(), SortOrder::Asc)
+        };
+        SortByField { field_name, order }
+    }
+}
+
+/// Specifies how documents are sorted.
+/// In case of a tie, the documents are ordered according to descending `(split_id, segment_ord,
+/// doc_id)`.
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub enum SortBy {
     /// Sort by document ID.
     DocId,
@@ -63,4 +88,37 @@ pub enum SortBy {
         /// Order to sort by. A usual top-k search implies a descending order.
         order: SortOrder,
     },
+}
+
+impl Default for SortBy {
+    fn default() -> Self {
+        Self::DocId
+    }
+}
+
+impl From<&SearchRequest> for SortBy {
+    fn from(req: &SearchRequest) -> Self {
+        if let Some(ref sort_by_field) = req.sort_by_field {
+            SortBy::FastField {
+                field_name: sort_by_field.to_string(),
+                order: req
+                    .sort_order
+                    .map(|sort_order| sort_order.into())
+                    .unwrap_or_default(),
+            }
+        } else {
+            SortBy::DocId
+        }
+    }
+}
+
+// Quickwit Proto SortOrder
+impl From<i32> for SortOrder {
+    fn from(sort: i32) -> Self {
+        match sort {
+            0 => Self::Asc,
+            1 => Self::Desc,
+            _ => panic!("unexpected value {}", sort),
+        }
+    }
 }
