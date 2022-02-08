b7488831b3ccf9915c634ae743626e606b8807f7
Paul Masurel <paul@quickwit.io>
2022-02-08 09:47:58 +0900

    Update quickwit-search/src/retry/search_stream.rs

--- quickwit-search/src/retry/search_stream.rs
+++ quickwit-search/src/retry/search_stream.rs
@@ -131,6 +131,6 @@ mod tests {
         );
         assert!(retry_req_opt.is_some());
         assert_eq!(retry_req_opt.as_ref().unwrap().split_offsets.len(), 1);
-        assert_eq!(&retry_req_opt.as_ref().unwrap().split_offsets[0].split_id == "split_2");
+        assert_eq!(&retry_req_opt.as_ref().unwrap().split_offsets[0].split_id, "split_2");
     }
 }
