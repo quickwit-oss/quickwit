21b3158324b7f391b84715fa036807c0db4045bd
Evance Soumaoro <evanxg852000@gmail.com>
2022-04-07 11:15:28 +0000

    started unittests

--- quickwit-pushapi/src/pushapi_source.rs
+++ quickwit-pushapi/src/pushapi_source.rs
@@ -17,15 +17,14 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-
 use async_trait::async_trait;
 use quickwit_actors::{ActorExitStatus, Mailbox};
 use quickwit_indexing::actors::Indexer;
 use quickwit_indexing::models::RawDocBatch;
 use quickwit_indexing::source::{Source, SourceContext};
-use quickwit_metastore::checkpoint::{CheckpointDelta, PartitionId, Position};
+use quickwit_metastore::checkpoint::{CheckpointDelta, PartitionId, Position, SourceCheckpoint};
 use quickwit_proto::push_api::{FetchRequest, FetchResponse};
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use tracing::info;
 
 use crate::{iter_doc_payloads, PushApiService};
@@ -39,7 +38,7 @@ pub struct PushApiSourceTracker {
     pub num_docs_processed: u64,
 }
 
-#[derive(Clone, Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct PushApiSourceParams {
     pub index_id: String,
 }
@@ -48,6 +47,33 @@ pub struct PushApiSource {
     params: PushApiSourceParams,
     tracker: PushApiSourceTracker,
     push_api_mailbox: Mailbox<PushApiService>,
+}
+
+impl PushApiSource {
+    pub fn make(
+        push_api_mailbox: Mailbox<PushApiService>,
+        index_id: String,
+        checkpoint: SourceCheckpoint,
+    ) -> anyhow::Result<Self> {
+        let partition_id = PartitionId::from(index_id.clone());
+        let offset = if let Some(Position::Offset(offset_str)) =
+            checkpoint.position_for_partition(&partition_id).cloned()
+        {
+            offset_str.parse::<u64>()?
+        } else {
+            0
+        };
+
+        let push_api_source = PushApiSource {
+            tracker: PushApiSourceTracker {
+                current_offset: offset,
+                num_docs_processed: 0,
+            },
+            params: PushApiSourceParams { index_id },
+            push_api_mailbox,
+        };
+        Ok(push_api_source)
+    }
 }
 
 #[async_trait]
@@ -123,4 +149,108 @@ impl Source for PushApiSource {
     }
 }
 
+// pub struct PushApiSourceFactory;
 
+// #[async_trait]
+// impl TypedSourceFactory for PushApiSourceFactory {
+//     type Source = PushApiSource;
+//     type Params = PushApiSourceParams;
+
+//     // TODO handle checkpoint for files.
+//     async fn typed_create_source(
+//         params: PushApiSourceParams,
+//         checkpoint: quickwit_metastore::checkpoint::SourceCheckpoint,
+//     ) -> anyhow::Result<Self::Source> {
+//         let partition_id = PartitionId::from(params.index_id);
+//         let offset = if let Some(Position::Offset(offset_str)) =
+//             checkpoint.position_for_partition(&partition_id).cloned()
+//         {
+//             offset_str.parse::<u64>()?
+//         } else {
+//             0
+//         };
+
+//         let push_api_source = PushApiSource {
+//             tracker: PushApiSourceTracker {
+//                 current_offset: offset,
+//                 num_docs_processed: 0,
+//             },
+//             params,
+//             push_api_mailbox: Mailbox<PushApiService>,
+//         };
+//         Ok(push_api_source)
+//     }
+// }
+
+#[cfg(test)]
+mod tests {
+    use quickwit_actors::{create_test_mailbox, Universe};
+    use quickwit_indexing::source::SourceActor;
+    use quickwit_metastore::checkpoint::SourceCheckpoint;
+    use quickwit_proto::push_api::{DocBatch, IngestRequest};
+
+    use super::*;
+    use crate::{add_doc, spawn_push_api_actor};
+
+    fn make_ingest_request(index_id: String) -> IngestRequest {
+        let mut doc_batch = DocBatch {
+            index_id,
+            ..Default::default()
+        };
+        for _ in 0..2000 {
+            add_doc(
+                r#"{"body": "It takes two to lie; one to lie ..."}"#.as_bytes(),
+                &mut doc_batch,
+            );
+        }
+        IngestRequest {
+            doc_batches: vec![doc_batch],
+        }
+    }
+
+    #[tokio::test]
+    async fn test_pushapi_source() -> anyhow::Result<()> {
+        // quickwit_common::setup_logging_for_tests();
+        let universe = Universe::new();
+        let index_id = "my-index".to_string();
+
+        let queue_path = tempfile::tempdir()?;
+
+        let push_api_service = spawn_push_api_actor(&universe, queue_path.path())?;
+
+        // add
+        let ingest_req = make_ingest_request(index_id.clone());
+        push_api_service
+            .ask_for_res(ingest_req)
+            .await
+            .map_err(|err| anyhow::anyhow!(err.to_string()))?;
+
+        let (mailbox, inbox) = create_test_mailbox();
+        let push_api_source =
+            PushApiSource::make(push_api_service, index_id, SourceCheckpoint::default())?;
+        let push_api_source_actor = SourceActor {
+            source: Box::new(push_api_source),
+            batch_sink: mailbox,
+        };
+        let (_push_api_source_mailbox, push_api_source_handle) =
+            universe.spawn_actor(push_api_source_actor).spawn();
+
+        let (actor_termination, tracker) = push_api_source_handle.join().await;
+        assert!(actor_termination.is_success());
+        assert_eq!(
+            tracker,
+            serde_json::json!({
+                "current_offset": 70u64,
+                "num_lines_processed": 4u32
+            })
+        );
+        let batch = inbox.drain_for_test();
+        assert_eq!(batch.len(), 2);
+        // assert!(matches!(
+        //     batch[1].downcast_ref::<Command>().unwrap(),
+        //     Command::ExitWithSuccess
+        // ));
+        Ok(())
+    }
+
+}
