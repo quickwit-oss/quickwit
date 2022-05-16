9024ce7f4a28f8f51dc4603d0284392def583bbe
Paul Masurel <paul@quickwit.io>
2022-05-16 14:30:40 +0900

    Bugfix: tokenizer for default `QuickwitJsonOption` should be default.
    Following #1393, the default tokenizer given when deserializing
    JsonOptions (the tokenizer property is missing), and the
    Default::default implementation are inconsistent.
    
    This is a problem because Default::default is used when deserializing
    the doc mapping if the entire `QuickwitJsonOption` is missing.

--- quickwit-doc-mapper/src/doc_mapper.rs
+++ quickwit-doc-mapper/src/doc_mapper.rs
@@ -109,10 +109,10 @@ clone_trait_object!(DocMapper);
 #[cfg(test)]
 mod tests {
     use quickwit_proto::SearchRequest;
-    use tantivy::schema::Cardinality;
+    use tantivy::schema::{Cardinality, FieldType};
 
     use crate::default_doc_mapper::{FieldMappingType, QuickwitJsonOptions};
-    use crate::{DefaultDocMapperBuilder, DocMapper, FieldMappingEntry};
+    use crate::{DefaultDocMapperBuilder, DocMapper, FieldMappingEntry, DYNAMIC_FIELD_NAME};
 
     const JSON_DEFAULT_DOC_MAPPER: &str = r#"
         {
@@ -132,6 +132,30 @@ mod tests {
             format!("{:?}", expected_default_doc_mapper),
         );
         Ok(())
+    }
+
+    #[test]
+    fn test_deserialize_minimal_doc_mapper() -> anyhow::Result<()> {
+        let deserialized_default_doc_mapper = serde_json::from_str::<Box<dyn DocMapper>>(r#"{"type": "default"}"#)?;
+        let expected_default_doc_mapper = DefaultDocMapperBuilder::default().try_build()?;
+        assert_eq!(
+            format!("{:?}", deserialized_default_doc_mapper),
+            format!("{:?}", expected_default_doc_mapper),
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_deserialize_doc_mapper_default_dynamic_tokenizer() {
+        let expected_default_doc_mapper = DefaultDocMapperBuilder::default().try_build().unwrap();
+        let tantivy_schema = expected_default_doc_mapper.schema();
+        let dynamic_field = tantivy_schema.get_field(DYNAMIC_FIELD_NAME).unwrap();
+        if let FieldType::JsonObject(json_options) = tantivy_schema.get_field_entry(dynamic_field).field_type() {
+            let text_opt = json_options.get_text_indexing_options().unwrap();
+            assert_eq!(text_opt.tokenizer(), "default");
+        } else {
+            panic!("dynamic field should be of JSON type");
+        }
     }
 
     #[test]
