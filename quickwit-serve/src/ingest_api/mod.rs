85299b124ffce3031e829570cb11cad77d9db43d
Evance Soumaoro <evanxg852000@gmail.com>
2022-05-16 07:28:56 +0000

    removed bulk endpoint

--- quickwit-serve/src/ingest_api/mod.rs
+++ quickwit-serve/src/ingest_api/mod.rs
@@ -20,6 +20,4 @@
 mod grpc_adapter;
 mod rest_handler;
 
-pub use rest_handler::{
-    bulk_handler, elastic_bulk_handler, ingest_handler, tail_handler, BulkApiError,
-};
+pub use rest_handler::{elastic_bulk_handler, ingest_handler, tail_handler, BulkApiError};
