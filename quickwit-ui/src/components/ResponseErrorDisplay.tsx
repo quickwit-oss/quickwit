b9ede8d09d4c1cd83beeb64034cae1f8b27ed64a
François Massot <francois.massot@gmail.com>
2022-05-20 00:06:24 +0200

    Add test on IndexesView.

--- quickwit-ui/src/components/ResponseErrorDisplay.tsx
+++ quickwit-ui/src/components/ResponseErrorDisplay.tsx
@@ -21,25 +21,26 @@ import { Box } from '@mui/material';
 import { ResponseError } from '../utils/models';
 import SentimentVeryDissatisfiedIcon from '@mui/icons-material/SentimentVeryDissatisfied';
 
-export default function ErrorResponseDisplay(error: ResponseError) {
-  const message = () => {
-    if (error.message !== null && error.message.includes('No search node available.')) {
-      return <Box sx={{fontSize: 16, pt: 2, }}>
-          Your cluster does not contain any search node. You need at least one search node.
-        </Box>
-    } else {
-      return <>
-        <Box sx={{fontSize: 16, pt: 2, }}>
-          {error.status && <span>Status: {error.status}</span>}
-        </Box>
-        <Box sx={{ fontSize: 14, pt: 1, alignItems: 'center'}}>
-          Error: {message}
-        </Box>
-      </>
-    }
+function renderMessage(error: ResponseError) {
+  if (error.message !== null && error.message.includes('No search node available.')) {
+    return <Box sx={{fontSize: 16, pt: 2, }}>
+        Your cluster does not contain any search node. You need at least one search node.
+      </Box>
+  } else {
+    return <>
+      <Box sx={{fontSize: 16, pt: 2, }}>
+        {error.status && <span>Status: {error.status}</span>}
+      </Box>
+      <Box sx={{ fontSize: 14, pt: 1, alignItems: 'center'}}>
+        Error: {error.message}
+      </Box>
+    </>
   }
+}
+
+export default function ErrorResponseDisplay(error: ResponseError) {
   return <Box sx={{ pt: 2, display: 'flex', flexDirection: 'column', alignItems: 'center' }} >
     <SentimentVeryDissatisfiedIcon sx={{ fontSize: 60 }} />
-    {message()}
+    {renderMessage(error)}
   </Box>
 }
