1b27d4ffc7090061668235cbfaa4a76d3d140353
François Massot <francois.massot@gmail.com>
2022-05-19 16:07:06 +0200

    Add custom message when a user tries to search in the UI with no search node in the cluster.

--- quickwit-ui/src/components/ResponseErrorDisplay.tsx
+++ quickwit-ui/src/components/ResponseErrorDisplay.tsx
@@ -22,13 +22,24 @@ import { ResponseError } from '../utils/models';
 import SentimentVeryDissatisfiedIcon from '@mui/icons-material/SentimentVeryDissatisfied';
 
 export default function ErrorResponseDisplay(error: ResponseError) {
+  const message = () => {
+    if (error.message !== null && error.message.includes('No search node available.')) {
+      return <Box sx={{fontSize: 16, pt: 2, }}>
+          Your cluster does not contain any search node. You need at least one search node.
+        </Box>
+    } else {
+      return <>
+        <Box sx={{fontSize: 16, pt: 2, }}>
+          {error.status && <span>Status: {error.status}</span>}
+        </Box>
+        <Box sx={{ fontSize: 14, pt: 1, alignItems: 'center'}}>
+          Error: {message}
+        </Box>
+      </>
+    }
+  }
   return <Box sx={{ pt: 2, display: 'flex', flexDirection: 'column', alignItems: 'center' }} >
     <SentimentVeryDissatisfiedIcon sx={{ fontSize: 60 }} />
-      <Box sx={{fontSize: 16, pt: 2, }}>
-        {error.status && <span>Status: {error.status}</span>}
-      </Box>
-      <Box sx={{ fontSize: 14, pt: 1, alignItems: 'center'}}>
-        Error: {error.message}
-      </Box>
-    </Box>
+    {message()}
+  </Box>
 }
