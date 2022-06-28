38880c9b2a36461104b3fec14a016cc0f7a5612c
PSeitz <PSeitz@users.noreply.github.com>
2022-06-28 17:48:17 +0800

    update tantivy (#1682)

--- quickwit-search/src/lib.rs
+++ quickwit-search/src/lib.rs
@@ -229,7 +229,7 @@ pub async fn single_node_search(
         let res: IntermediateAggregationResults =
             serde_json::from_str(&intermediate_aggregation_result)?;
         let req: Aggregations = serde_json::from_str(search_request.aggregation_request())?;
-        let res: AggregationResults = AggregationResults::from_intermediate_and_req(res, req)?;
+        let res: AggregationResults = res.into_final_bucket_result(req)?;
         Some(serde_json::to_string(&res)?)
     } else {
         None
