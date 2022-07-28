15b5059a86d1d0fb50a1714bad71ca018fc4ffae
Kimbro Staken <kstaken@terascope.io>
2022-07-28 14:17:57 -0700

    Improve the reliability of the localstack container startup for tests

--- .localstack/init.sh
+++ .localstack/init.sh
@@ -2,6 +2,9 @@
 
 set -eu
 
-awslocal kinesis create-stream --stream-name quickwit-dev-stream --shard-count 3
 awslocal s3 mb s3://quickwit-dev
 awslocal s3 mb s3://quickwit-integration-tests && awslocal s3 rm --recursive s3://quickwit-integration-tests
+
+if ! awslocal kinesis list-streams | grep quickwit-dev-stream ; then
+    awslocal kinesis create-stream --stream-name quickwit-dev-stream --shard-count 3
+fi
