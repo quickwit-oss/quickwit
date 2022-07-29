b305b9a8144a2602da72d698a3ca6ba9026fd612
Kimbro Staken <kstaken@terascope.io>
2022-07-29 16:10:35 -0700

    Switching to docker compose and adding --wait option

--- .localstack/init.sh
+++ .localstack/init.sh
@@ -5,6 +5,6 @@ set -eu
 awslocal s3 mb s3://quickwit-dev
 awslocal s3 mb s3://quickwit-integration-tests && awslocal s3 rm --recursive s3://quickwit-integration-tests
 
-if ! awslocal kinesis list-streams | grep quickwit-dev-stream ; then
+if ! awslocal kinesis list-streams | grep -q quickwit-dev-stream ; then
     awslocal kinesis create-stream --stream-name quickwit-dev-stream --shard-count 3
 fi
