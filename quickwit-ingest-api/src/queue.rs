68748c806a8d9931ab7f7b1e2ac3fc1809dbe180
Adrien Guillo <adrien.guillo@gmail.com>
2022-08-06 18:44:41 -0400

    Adapt code to new iterator API

--- quickwit-ingest-api/src/queue.rs
+++ quickwit-ingest-api/src/queue.rs
@@ -59,11 +59,13 @@ fn next_position(db: &DB, queue_id: &str) -> crate::Result<Option<Position>> {
         })?;
     // That's iterating backward
     let mut full_it = db.full_iterator_cf(&cf, IteratorMode::End);
-    if let Some((key, _)) = full_it.next() {
-        let position = Position::try_from(&*key)?;
-        Ok(Some(position))
-    } else {
-        Ok(None)
+    match full_it.next() {
+        Some(Ok((key, _))) => {
+            let position = Position::try_from(&*key)?;
+            Ok(Some(position))
+        }
+        Some(Err(error)) => Err(error.into()),
+        None => Ok(None),
     }
 }
 
@@ -235,7 +237,8 @@ impl Queues {
         let mut num_bytes = 0;
         let mut first_key_opt: Option<u64> = None;
         let size_limit = num_bytes_limit.unwrap_or(FETCH_PAYLOAD_LIMIT);
-        for (key, payload) in full_it {
+        for kp_res in full_it {
+            let (key, payload) = kp_res?;
             let position = Position::try_from(&*key)?;
             if first_key_opt.is_none() {
                 first_key_opt = Some(position.into());
@@ -263,7 +266,8 @@ impl Queues {
         let mut doc_batch = DocBatch::default();
         let mut num_bytes = 0;
         let mut first_key_opt: Option<u64> = None;
-        for (key, payload) in full_it {
+        for kp_res in full_it {
+            let (key, payload) = kp_res?;
             let position = Position::try_from(&*key)?;
             if first_key_opt.is_none() {
                 first_key_opt = Some(position.into());
