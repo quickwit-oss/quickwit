9551ec329710c515aefde9f158ac34fb163115ab
Adrien Guillo <adrien.guillo@gmail.com>
2022-09-20 19:45:05 -0400

    Move .license_header file to quickwit source dir

--- quickwit/scripts/check_license_headers.sh
+++ quickwit/scripts/check_license_headers.sh
@@ -4,7 +4,7 @@ RESULT=0
 
 for file in $(git ls-files | grep "src\|proto" | grep -e "\.proto\|\.rs\|\.ts" | grep -v "quickwit-proto/otlp" | grep -v "quickwit-proto/src")
 do
-    diff <(sed 's/{\\d+}/2022/' ../.license_header.txt) <(head -n 19 $file)
+    diff <(sed 's/{\\d+}/2022/' .license_header.txt) <(head -n 19 $file)
     DIFFRESULT=$?
     if [ $DIFFRESULT -ne 0 ]; then
         echo $DIFFRESULT
