193dd6045d6f3b6186a750b37415164ddf182c72
boraarslan <bora-arslan@outlook.com>
2022-09-28 13:59:51 +0300

    make fmt

--- quickwit/quickwit-cli/tests/helpers.rs
+++ quickwit/quickwit-cli/tests/helpers.rs
@@ -18,7 +18,6 @@
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 use std::collections::HashMap;
-
 use std::path::PathBuf;
 use std::process::{Child, Stdio};
 use std::sync::Arc;
