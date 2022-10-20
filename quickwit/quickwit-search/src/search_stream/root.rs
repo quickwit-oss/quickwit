eeca9879d91f84d7fc3d95bb62b0c3c5a212df28
ChillFish8 <hburt2003@gmail.com>
2022-10-20 16:29:24 +0200

    Fix clippy errors

--- quickwit/quickwit-search/src/search_stream/root.rs
+++ quickwit/quickwit-search/src/search_stream/root.rs
@@ -101,11 +101,10 @@ fn jobs_to_leaf_request(
 
 #[cfg(test)]
 mod tests {
-    use std::ops::Range;
     use std::sync::Arc;
 
     use quickwit_indexing::mock_split;
-    use quickwit_metastore::{IndexMetadata, MockMetastore, SplitState};
+    use quickwit_metastore::{IndexMetadata, MockMetastore};
     use quickwit_proto::OutputFormat;
     use tokio_stream::wrappers::UnboundedReceiverStream;
 
