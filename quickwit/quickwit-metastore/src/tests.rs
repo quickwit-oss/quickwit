11aa168b1b8d75044a6f1bf13425a381fe79955b
ChillFish8 <hburt2003@gmail.com>
2022-10-21 16:26:50 +0200

    Reformat code

--- quickwit/quickwit-metastore/src/tests.rs
+++ quickwit/quickwit-metastore/src/tests.rs
@@ -2014,7 +2014,7 @@ pub mod test_suite {
             .stage_split(index_id, split_metadata.clone())
             .await
             .unwrap();
-        
+
         sleep(Duration::from_secs(1)).await;
         let split_meta = metastore.list_all_splits(index_id).await.unwrap()[0].clone();
         assert!(split_meta.update_timestamp > current_timestamp);
