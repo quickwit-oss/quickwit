84b38c7a017aa1263b29e46f5e8945761766899a
ChillFish8 <hburt2003@gmail.com>
2022-10-21 15:58:16 +0200

    Remove trigger code

--- quickwit/quickwit-metastore/migrations/postgresql/2_create-splits.up.sql
+++ quickwit/quickwit-metastore/migrations/postgresql/2_create-splits.up.sql
@@ -13,27 +13,6 @@ CREATE TABLE IF NOT EXISTS splits (
 );
 
 DROP TRIGGER IF EXISTS quickwit_set_index_update_timestamp_on_split_change ON splits CASCADE;
-CREATE TRIGGER quickwit_set_index_update_timestamp_on_split_change
-    AFTER INSERT OR DELETE OR UPDATE ON splits
-    FOR EACH STATEMENT
-    EXECUTE PROCEDURE set_index_update_timestamp_for_split();
-
--- We also want to update an index `update_timestamp` field whenever a related split
--- is modified.
-CREATE OR REPLACE FUNCTION set_index_update_timestamp_for_split() RETURNS trigger AS $$
-BEGIN
-    IF (TG_OP = 'INSERT' OR TG_OP = 'UPDATE') THEN
-        UPDATE indexes SET update_timestamp = NEW.update_timestamp
-        WHERE indexes.index_id = NEW.index_id;
-    ELSIF (TG_OP = 'DELETE') THEN
-        UPDATE indexes SET update_timestamp = (CURRENT_TIMESTAMP AT TIME ZONE 'UTC')
-        WHERE indexes.index_id = OLD.index_id;
-    END IF;
-    RETURN NULL;
-END;
-
-$$ LANGUAGE plpgsql;
-
 
 -- apply the trigger to the `splits` table
 SELECT quickwit_manage_update_timestamp('splits');
