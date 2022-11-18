817545bb7615f6ea16c834e8c9370cde38208acc
François Massot <francois.massot@gmail.com>
2022-11-18 12:21:40 +0100

    Clean search grpc adpater.

--- quickwit/quickwit-serve/src/search_api/grpc_adapter.rs
+++ quickwit/quickwit-serve/src/search_api/grpc_adapter.rs
@@ -21,39 +21,12 @@ use std::sync::Arc;
 
 use async_trait::async_trait;
 use futures::TryStreamExt;
-use opentelemetry::global;
-use opentelemetry::propagation::Extractor;
 use quickwit_proto::{
     convert_to_grpc_result, search_service_server as grpc, set_parent_span_from_request_metadata,
     tonic, LeafSearchStreamRequest, LeafSearchStreamResponse, ServiceError,
 };
 use quickwit_search::SearchService;
-use tracing::{instrument, Span};
-use tracing_opentelemetry::OpenTelemetrySpanExt;
-
-// The `MetadataMap` thing here is used to extract open telemetry
-// tracing keys from request's headers.
-
-struct MetadataMap<'a>(&'a tonic::metadata::MetadataMap);
-
-impl<'a> Extractor for MetadataMap<'a> {
-    /// Gets a value for a key from the MetadataMap.  If the value can't be converted to &str,
-    /// returns None
-    fn get(&self, key: &str) -> Option<&str> {
-        self.0.get(key).and_then(|metadata| metadata.to_str().ok())
-    }
-
-    /// Collect all the keys from the MetadataMap.
-    fn keys(&self) -> Vec<&str> {
-        self.0
-            .keys()
-            .map(|key| match key {
-                tonic::metadata::KeyRef::Ascii(v) => v.as_str(),
-                tonic::metadata::KeyRef::Binary(v) => v.as_str(),
-            })
-            .collect::<Vec<_>>()
-    }
-}
+use tracing::instrument;
 
 #[derive(Clone)]
 pub struct GrpcSearchAdapter(Arc<dyn SearchService>);
@@ -71,9 +44,7 @@ impl grpc::SearchService for GrpcSearchAdapter {
         &self,
         request: tonic::Request<quickwit_proto::SearchRequest>,
     ) -> Result<tonic::Response<quickwit_proto::SearchResponse>, tonic::Status> {
-        let parent_cx =
-            global::get_text_map_propagator(|prop| prop.extract(&MetadataMap(request.metadata())));
-        Span::current().set_parent(parent_cx);
+        set_parent_span_from_request_metadata(request.metadata());
         let search_request = request.into_inner();
         let search_res = self.0.root_search(search_request).await;
         convert_to_grpc_result(search_res)
