ec79772b5d7240dadc5daf7dc5e46665273345c7
Adrien Guillo <adrien@quickwit.io>
2022-11-22 10:53:00 +0100

    Upgrade Rust dependencies (#2377)

--- quickwit/quickwit-proto/src/opentelemetry.proto.resource.v1.rs
+++ quickwit/quickwit-proto/src/opentelemetry.proto.resource.v1.rs
@@ -5,10 +5,10 @@ pub struct Resource {
     /// Set of attributes that describe the resource.
     /// Attribute keys MUST be unique (it is not allowed to have more than one
     /// attribute with the same key).
-    #[prost(message, repeated, tag="1")]
+    #[prost(message, repeated, tag = "1")]
     pub attributes: ::prost::alloc::vec::Vec<super::super::common::v1::KeyValue>,
     /// dropped_attributes_count is the number of dropped attributes. If the value is 0, then
     /// no attributes were dropped.
-    #[prost(uint32, tag="2")]
+    #[prost(uint32, tag = "2")]
     pub dropped_attributes_count: u32,
 }
