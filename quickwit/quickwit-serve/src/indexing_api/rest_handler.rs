34db93a5c88c8903f016d37e883c3b4d4ece9087
ChillFish8 <hburt2003@gmail.com>
2023-01-09 11:09:30 +0100

    Fix typo

--- quickwit/quickwit-serve/src/indexing_api/rest_handler.rs
+++ quickwit/quickwit-serve/src/indexing_api/rest_handler.rs
@@ -34,7 +34,7 @@ pub struct IndexingApi;
     tag = "Indexing",
     path = "/indexing",
     responses(
-        (status = 200, description = "Successfully observed indexing pipeline.", body = Object)
+        (status = 200, description = "Successfully observed indexing pipelines.", body = Object)
     ),
 )]
 /// Observe Indexing Pipeline
