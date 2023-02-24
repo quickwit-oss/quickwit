7b150b639299d396e0132916b8ab8418303ad7fc
François Massot <francois.massot@gmail.com>
2023-02-24 18:03:53 +0100

    Fix format.

--- quickwit/quickwit-cli/src/source.rs
+++ quickwit/quickwit-cli/src/source.rs
@@ -414,7 +414,10 @@ async fn toggle_source_cli(args: ToggleSourceArgs) -> anyhow::Result<()> {
 
 pub async fn toggle_ingest_api_cli(args: ToggleIngestApiArgs) -> anyhow::Result<()> {
     debug!(args=?args, "toggle-ingest-api");
-    println!("❯ {}abling ingest API...", if args.enabled { "En" } else { "Dis" });
+    println!(
+        "❯ {}abling ingest API...",
+        if args.enabled { "En" } else { "Dis" }
+    );
     let transport = Transport::new(args.cluster_endpoint);
     let qw_client = QuickwitClient::new(transport);
     qw_client
