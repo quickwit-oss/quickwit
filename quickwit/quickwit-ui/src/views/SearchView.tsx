ed3661fbbe5533a72f70d49ba6343645a07e800b
François Massot <francois.massot@gmail.com>
2023-03-17 09:18:01 +0100

    Fix index side bar width and index combobox.

--- quickwit/quickwit-ui/src/views/SearchView.tsx
+++ quickwit/quickwit-ui/src/views/SearchView.tsx
@@ -42,6 +42,9 @@ function updateSearchRequestWithIndex(index: Index | null, searchRequest: Search
     searchRequest.startTimestamp = null;
     searchRequest.endTimestamp = null;
   }
+  if (index?.metadata.index_config.index_id) {
+    searchRequest.indexId = index?.metadata.index_config.index_id;
+  }
 }
 
 function SearchView() {
@@ -66,8 +69,8 @@ function SearchView() {
     setSearchRequest(updatedSearchRequest);
     setQueryRunning(true);
     setSearchError(null);
+    navigate('/search?' + toUrlSearchRequestParams(updatedSearchRequest).toString());
     quickwitClient.search(updatedSearchRequest).then((response) => {
-      navigate('/search?' + toUrlSearchRequestParams(updatedSearchRequest).toString());
       updateLastSearchRequest(updatedSearchRequest);
       setSearchResponse(response);
       setQueryRunning(false);
@@ -80,7 +83,6 @@ function SearchView() {
   const onIndexMetadataUpdate = (indexMetadata: IndexMetadata | null) => {
     setSearchRequest(previousRequest => {
       updateSearchRequestWithIndex(index, previousRequest);
-      setSearchRequest(previousRequest);
       return {...previousRequest, indexId: indexMetadata === null ? null : indexMetadata.index_config.index_id};
     });
   }
