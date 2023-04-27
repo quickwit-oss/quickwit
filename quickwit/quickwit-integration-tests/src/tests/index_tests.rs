83f83cb6fbb8a8eaf12e8a3f3519da3c78730f91
Igor Motov <igor@motovs.org>
2023-04-26 15:06:20 -1000

    Fix format

--- quickwit/quickwit-integration-tests/src/tests/index_tests.rs
+++ quickwit/quickwit-integration-tests/src/tests/index_tests.rs
@@ -17,8 +17,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-
-
 use bytes::Bytes;
 use quickwit_metastore::SplitState;
 use quickwit_rest_client::rest_client::CommitType;
