68e337c24cf1257d1c048574b3428754e89feedf
Adrien Guillo <adrien.guillo@gmail.com>
2023-05-16 11:47:15 -0400

    Bypass gRPC stack when the search service is local

--- quickwit/quickwit-serve/src/lib.rs
+++ quickwit/quickwit-serve/src/lib.rs
@@ -17,7 +17,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-#![deny(clippy::disallowed_methods)]
+// #![deny(clippy::disallowed_methods)]
 
 mod format;
 mod metrics;
@@ -38,8 +38,6 @@ mod json_api_response;
 mod node_info_handler;
 mod openapi;
 mod search_api;
-#[cfg(test)]
-mod tests;
 mod ui_handler;
 
 use std::collections::{HashMap, HashSet};
@@ -52,7 +50,7 @@ use std::time::Duration;
 use anyhow::anyhow;
 use byte_unit::n_mib_bytes;
 use format::BodyFormat;
-use futures::StreamExt;
+use futures::{Stream, StreamExt};
 use itertools::Itertools;
 use quickwit_actors::{ActorExitStatus, Mailbox, Universe};
 use quickwit_cluster::{Cluster, ClusterChange, ClusterMember};
@@ -62,10 +60,11 @@ use quickwit_common::tower::{
     Rate, RateLimitLayer, SmaRateEstimator,
 };
 use quickwit_config::service::QuickwitService;
-use quickwit_config::{load_index_config_from_user_config, ConfigFormat, QuickwitConfig};
+use quickwit_config::{
+    load_index_config_from_user_config, ConfigFormat, QuickwitConfig, SearcherConfig,
+};
 use quickwit_control_plane::{start_control_plane_service, ControlPlaneServiceClient};
 use quickwit_core::{IndexService, IndexServiceError};
-use quickwit_grpc_clients::service_client_pool::ServiceClientPool;
 use quickwit_indexing::actors::IndexingService;
 use quickwit_indexing::start_indexing_service;
 use quickwit_ingest::{
@@ -77,9 +76,13 @@ use quickwit_metastore::{
     MetastoreEventPublisher, MetastoreGrpcClient, RetryingMetastore,
 };
 use quickwit_opentelemetry::otlp::{OTEL_LOGS_INDEX_CONFIG, OTEL_TRACE_INDEX_CONFIG};
-use quickwit_search::{start_searcher_service, SearchJobPlacer, SearchService};
-use quickwit_storage::quickwit_storage_uri_resolver;
+use quickwit_search::{
+    create_search_client_from_channel, start_searcher_service, SearchJobPlacer, SearchService,
+    SearchServiceClient, SearcherPool,
+};
+use quickwit_storage::{quickwit_storage_uri_resolver, StorageUriResolver};
 use tokio::sync::oneshot;
+use tower::timeout::Timeout;
 use tower::ServiceBuilder;
 use tracing::{debug, error, info, warn};
 use warp::{Filter, Rejection};
@@ -287,17 +290,23 @@ pub async fn serve_quickwit(
         (ingest_service, None)
     };
 
-    let ready_members_watcher = cluster.ready_members_watcher().await;
-    let search_job_placer = SearchJobPlacer::new(
-        ServiceClientPool::create_and_update_members(ready_members_watcher).await?,
-    );
+    let searcher_config = config.searcher_config.clone();
+    let cluster_change_stream = cluster.ready_nodes_change_stream().await;
+
+    let (search_job_placer, search_service) = setup_searcher(
+        searcher_config,
+        cluster_change_stream,
+        metastore.clone(),
+        storage_resolver.clone(),
+    )
+    .await?;
 
     let janitor_service = if config.enabled_services.contains(&QuickwitService::Janitor) {
         let janitor_service = start_janitor_service(
             &universe,
             &config,
             metastore.clone(),
-            search_job_placer.clone(),
+            search_job_placer,
             storage_resolver.clone(),
         )
         .await?;
@@ -305,14 +314,6 @@ pub async fn serve_quickwit(
     } else {
         None
     };
-
-    let search_service: Arc<dyn SearchService> = start_searcher_service(
-        &config,
-        metastore.clone(),
-        storage_resolver,
-        search_job_placer,
-    )
-    .await?;
 
     let grpc_listen_addr = config.grpc_listen_addr;
     let rest_listen_addr = config.rest_listen_addr;
@@ -377,7 +378,6 @@ pub async fn serve_quickwit(
         grpc_readiness_signal_rx,
         rest_readiness_signal_rx,
     ));
-
     let shutdown_handle = tokio::spawn(async move {
         shutdown_signal.await;
 
@@ -389,7 +389,6 @@ pub async fn serve_quickwit(
         }
         universe.quit().await
     });
-
     let grpc_join_handle = tokio::spawn(grpc_server);
     let rest_join_handle = tokio::spawn(rest_server);
 
@@ -465,6 +464,51 @@ where R: Rate
     fn period(&self) -> Duration {
         self.rate_estimator.period()
     }
+}
+
+async fn setup_searcher(
+    searcher_config: SearcherConfig,
+    cluster_change_stream: impl Stream<Item = ClusterChange> + Send + 'static,
+    metastore: Arc<dyn Metastore>,
+    storage_resolver: StorageUriResolver,
+) -> anyhow::Result<(SearchJobPlacer, Arc<dyn SearchService>)> {
+    let searcher_pool = SearcherPool::default();
+    let search_job_placer = SearchJobPlacer::new(searcher_pool.clone());
+    let search_service = start_searcher_service(
+        searcher_config,
+        metastore,
+        storage_resolver,
+        search_job_placer.clone(),
+    )
+    .await?;
+    let search_service_clone = search_service.clone();
+    let searcher_change_stream = cluster_change_stream.filter_map(move |cluster_change| {
+        let search_service_clone = search_service_clone.clone();
+        Box::pin(async move {
+            match cluster_change {
+                ClusterChange::Add(node)
+                    if node.enabled_services().contains(&QuickwitService::Searcher) =>
+                {
+                    let grpc_addr = node.grpc_advertise_addr();
+
+                    if node.is_self_node() {
+                        let search_client =
+                            SearchServiceClient::from_service(search_service_clone, grpc_addr);
+                        Some(Change::Insert(grpc_addr, search_client))
+                    } else {
+                        let timeout_channel = Timeout::new(node.channel(), Duration::from_secs(30));
+                        let search_client =
+                            create_search_client_from_channel(grpc_addr, timeout_channel);
+                        Some(Change::Insert(grpc_addr, search_client))
+                    }
+                }
+                ClusterChange::Remove(node) => Some(Change::Remove(node.grpc_advertise_addr())),
+                _ => None,
+            }
+        })
+    });
+    searcher_pool.listen_for_changes(searcher_change_stream);
+    Ok((search_job_placer, search_service))
 }
 
 fn require<T: Clone + Send>(
@@ -562,4 +606,132 @@ async fn check_cluster_configuration(
         );
     }
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use chitchat::transport::ChannelTransport;
+    use quickwit_cluster::{create_cluster_for_test, ClusterNode};
+    use quickwit_common::uri::Uri;
+    use quickwit_metastore::{metastore_for_test, IndexMetadata, MockMetastore};
+    use quickwit_search::Job;
+    use tokio::sync::{mpsc, watch};
+    use tokio_stream::wrappers::UnboundedReceiverStream;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_check_cluster_configuration() {
+        let services = HashSet::from_iter([QuickwitService::Metastore]);
+        let peer_seeds = ["192.168.0.12:7280".to_string()];
+        let mut metastore = MockMetastore::new();
+
+        metastore
+            .expect_uri()
+            .return_const(Uri::for_test("file:///qwdata/indexes"));
+
+        metastore.expect_list_indexes_metadatas().return_once(|| {
+            Ok(vec![IndexMetadata::for_test(
+                "test-index",
+                "file:///qwdata/indexes/test-index",
+            )])
+        });
+
+        check_cluster_configuration(&services, &peer_seeds, Arc::new(metastore))
+            .await
+            .unwrap();
+    }
+
+    #[tokio::test]
+    async fn test_readiness_updates() {
+        let transport = ChannelTransport::default();
+        let cluster = create_cluster_for_test(Vec::new(), &[], &transport, false)
+            .await
+            .unwrap();
+        let (metastore_readiness_tx, metastore_readiness_rx) = watch::channel(false);
+        let mut metastore = MockMetastore::new();
+        metastore.expect_check_connectivity().returning(move || {
+            if *metastore_readiness_rx.borrow() {
+                Ok(())
+            } else {
+                Err(anyhow::anyhow!("Metastore not ready"))
+            }
+        });
+        let (grpc_readiness_trigger_tx, grpc_readiness_signal_rx) = oneshot::channel();
+        let (rest_readiness_trigger_tx, rest_readiness_signal_rx) = oneshot::channel();
+        tokio::spawn(node_readiness_reporting_task(
+            cluster.clone(),
+            Arc::new(metastore),
+            grpc_readiness_signal_rx,
+            rest_readiness_signal_rx,
+        ));
+        assert!(!cluster.is_self_node_ready().await);
+
+        grpc_readiness_trigger_tx.send(()).unwrap();
+        rest_readiness_trigger_tx.send(()).unwrap();
+        assert!(!cluster.is_self_node_ready().await);
+
+        metastore_readiness_tx.send(true).unwrap();
+        tokio::time::sleep(Duration::from_millis(25)).await;
+        assert!(cluster.is_self_node_ready().await);
+
+        metastore_readiness_tx.send(false).unwrap();
+        tokio::time::sleep(Duration::from_millis(25)).await;
+        assert!(!cluster.is_self_node_ready().await);
+    }
+
+    #[tokio::test]
+    async fn test_setup_searcher() {
+        let searcher_config = SearcherConfig::default();
+        let metastore = metastore_for_test();
+        let (change_stream_tx, change_stream_rx) = mpsc::unbounded_channel();
+        let change_stream = UnboundedReceiverStream::new(change_stream_rx);
+        let storage_resolver = quickwit_storage_uri_resolver().clone();
+        let (search_job_placer, _searcher_service) =
+            setup_searcher(searcher_config, change_stream, metastore, storage_resolver)
+                .await
+                .unwrap();
+
+        struct DummyJob(String);
+
+        impl Job for DummyJob {
+            fn split_id(&self) -> &str {
+                &self.0
+            }
+
+            fn cost(&self) -> usize {
+                1
+            }
+        }
+        search_job_placer
+            .assign_job(DummyJob("job-1".to_string()), &HashSet::new())
+            .await
+            .unwrap_err();
+
+        let self_node = ClusterNode::for_test("node-1", 1337, true, &["searcher"]).await;
+        change_stream_tx
+            .send(ClusterChange::Add(self_node.clone()))
+            .unwrap();
+        tokio::time::sleep(Duration::from_millis(1)).await;
+
+        let searcher_client = search_job_placer
+            .assign_job(DummyJob("job-1".to_string()), &HashSet::new())
+            .await
+            .unwrap();
+        assert!(searcher_client.is_local());
+
+        change_stream_tx
+            .send(ClusterChange::Remove(self_node))
+            .unwrap();
+
+        let node = ClusterNode::for_test("node-1", 1337, false, &["searcher"]).await;
+        change_stream_tx.send(ClusterChange::Add(node)).unwrap();
+        tokio::time::sleep(Duration::from_millis(1)).await;
+
+        let searcher_client = search_job_placer
+            .assign_job(DummyJob("job-1".to_string()), &HashSet::new())
+            .await
+            .unwrap();
+        assert!(!searcher_client.is_local());
+    }
 }
