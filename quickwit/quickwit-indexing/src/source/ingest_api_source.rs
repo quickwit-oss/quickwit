a2e67dd12b1f3ebf6397ab299e87ef1565c73338
Adrien Guillo <adrien.guillo@gmail.com>
2023-05-20 18:53:36 -0400

    Protect some futures in ingest source

--- quickwit/quickwit-indexing/src/source/ingest_api_source.rs
+++ quickwit/quickwit-indexing/src/source/ingest_api_source.rs
@@ -128,9 +128,8 @@ impl Source for IngestApiSource {
         let FetchResponse {
             first_position: first_position_opt,
             doc_batch: doc_batch_opt,
-        } = self
-            .ingest_api_service
-            .ask_for_res(fetch_req)
+        } = ctx
+            .protect_future(self.ingest_api_service.ask_for_res(fetch_req))
             .await
             .map_err(anyhow::Error::from)?;
 
@@ -178,7 +177,7 @@ impl Source for IngestApiSource {
     async fn suggest_truncate(
         &self,
         checkpoint: SourceCheckpoint,
-        _ctx: &ActorContext<SourceActor>,
+        ctx: &ActorContext<SourceActor>,
     ) -> anyhow::Result<()> {
         if let Some(Position::Offset(offset_str)) =
             checkpoint.position_for_partition(&self.partition_id)
@@ -188,8 +187,7 @@ impl Source for IngestApiSource {
                 index_id: self.ctx.index_id.clone(),
                 up_to_position_included,
             };
-            self.ingest_api_service
-                .ask_for_res(suggest_truncate_req)
+            ctx.protect_future(self.ingest_api_service.ask_for_res(suggest_truncate_req))
                 .await
                 .map_err(anyhow::Error::from)?;
         }
