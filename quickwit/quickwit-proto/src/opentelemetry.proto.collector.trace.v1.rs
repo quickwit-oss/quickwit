242ab97fd1f4c978d969a9740582648aa2b80fe0
Adrien Guillo <adrien.guillo@gmail.com>
2023-05-23 19:02:36 -0400

    Bump criterion, hyper-rustls, tonic, and opentelemetry crates

--- quickwit/quickwit-proto/src/opentelemetry.proto.collector.trace.v1.rs
+++ quickwit/quickwit-proto/src/opentelemetry.proto.collector.trace.v1.rs
@@ -70,7 +70,7 @@ pub mod trace_service_client {
         /// Attempt to create a new client by connecting to a given endpoint.
         pub async fn connect<D>(dst: D) -> Result<Self, tonic::transport::Error>
         where
-            D: std::convert::TryInto<tonic::transport::Endpoint>,
+            D: TryInto<tonic::transport::Endpoint>,
             D::Error: Into<StdError>,
         {
             let conn = tonic::transport::Endpoint::new(dst)?.connect().await?;
@@ -126,12 +126,31 @@ pub mod trace_service_client {
             self.inner = self.inner.accept_compressed(encoding);
             self
         }
+        /// Limits the maximum size of a decoded message.
+        ///
+        /// Default: `4MB`
+        #[must_use]
+        pub fn max_decoding_message_size(mut self, limit: usize) -> Self {
+            self.inner = self.inner.max_decoding_message_size(limit);
+            self
+        }
+        /// Limits the maximum size of an encoded message.
+        ///
+        /// Default: `usize::MAX`
+        #[must_use]
+        pub fn max_encoding_message_size(mut self, limit: usize) -> Self {
+            self.inner = self.inner.max_encoding_message_size(limit);
+            self
+        }
         /// For performance reasons, it is recommended to keep this RPC
         /// alive for the entire life of the application.
         pub async fn export(
             &mut self,
             request: impl tonic::IntoRequest<super::ExportTraceServiceRequest>,
-        ) -> Result<tonic::Response<super::ExportTraceServiceResponse>, tonic::Status> {
+        ) -> std::result::Result<
+            tonic::Response<super::ExportTraceServiceResponse>,
+            tonic::Status,
+        > {
             self.inner
                 .ready()
                 .await
@@ -145,7 +164,15 @@ pub mod trace_service_client {
             let path = http::uri::PathAndQuery::from_static(
                 "/opentelemetry.proto.collector.trace.v1.TraceService/Export",
             );
-            self.inner.unary(request.into_request(), path, codec).await
+            let mut req = request.into_request();
+            req.extensions_mut()
+                .insert(
+                    GrpcMethod::new(
+                        "opentelemetry.proto.collector.trace.v1.TraceService",
+                        "Export",
+                    ),
+                );
+            self.inner.unary(req, path, codec).await
         }
     }
 }
@@ -161,7 +188,10 @@ pub mod trace_service_server {
         async fn export(
             &self,
             request: tonic::Request<super::ExportTraceServiceRequest>,
-        ) -> Result<tonic::Response<super::ExportTraceServiceResponse>, tonic::Status>;
+        ) -> std::result::Result<
+            tonic::Response<super::ExportTraceServiceResponse>,
+            tonic::Status,
+        >;
     }
     /// Service that can be used to push spans between one Application instrumented with
     /// OpenTelemetry and a collector, or between a collector and a central collector (in this
@@ -171,6 +201,8 @@ pub mod trace_service_server {
         inner: _Inner<T>,
         accept_compression_encodings: EnabledCompressionEncodings,
         send_compression_encodings: EnabledCompressionEncodings,
+        max_decoding_message_size: Option<usize>,
+        max_encoding_message_size: Option<usize>,
     }
     struct _Inner<T>(Arc<T>);
     impl<T: TraceService> TraceServiceServer<T> {
@@ -183,6 +215,8 @@ pub mod trace_service_server {
                 inner,
                 accept_compression_encodings: Default::default(),
                 send_compression_encodings: Default::default(),
+                max_decoding_message_size: None,
+                max_encoding_message_size: None,
             }
         }
         pub fn with_interceptor<F>(
@@ -206,6 +240,22 @@ pub mod trace_service_server {
             self.send_compression_encodings.enable(encoding);
             self
         }
+        /// Limits the maximum size of a decoded message.
+        ///
+        /// Default: `4MB`
+        #[must_use]
+        pub fn max_decoding_message_size(mut self, limit: usize) -> Self {
+            self.max_decoding_message_size = Some(limit);
+            self
+        }
+        /// Limits the maximum size of an encoded message.
+        ///
+        /// Default: `usize::MAX`
+        #[must_use]
+        pub fn max_encoding_message_size(mut self, limit: usize) -> Self {
+            self.max_encoding_message_size = Some(limit);
+            self
+        }
     }
     impl<T, B> tonic::codegen::Service<http::Request<B>> for TraceServiceServer<T>
     where
@@ -219,7 +269,7 @@ pub mod trace_service_server {
         fn poll_ready(
             &mut self,
             _cx: &mut Context<'_>,
-        ) -> Poll<Result<(), Self::Error>> {
+        ) -> Poll<std::result::Result<(), Self::Error>> {
             Poll::Ready(Ok(()))
         }
         fn call(&mut self, req: http::Request<B>) -> Self::Future {
@@ -241,13 +291,15 @@ pub mod trace_service_server {
                             &mut self,
                             request: tonic::Request<super::ExportTraceServiceRequest>,
                         ) -> Self::Future {
-                            let inner = self.0.clone();
+                            let inner = Arc::clone(&self.0);
                             let fut = async move { (*inner).export(request).await };
                             Box::pin(fut)
                         }
                     }
                     let accept_compression_encodings = self.accept_compression_encodings;
                     let send_compression_encodings = self.send_compression_encodings;
+                    let max_decoding_message_size = self.max_decoding_message_size;
+                    let max_encoding_message_size = self.max_encoding_message_size;
                     let inner = self.inner.clone();
                     let fut = async move {
                         let inner = inner.0;
@@ -257,6 +309,10 @@ pub mod trace_service_server {
                             .apply_compression_config(
                                 accept_compression_encodings,
                                 send_compression_encodings,
+                            )
+                            .apply_max_message_size_config(
+                                max_decoding_message_size,
+                                max_encoding_message_size,
                             );
                         let res = grpc.unary(method, req).await;
                         Ok(res)
@@ -285,12 +341,14 @@ pub mod trace_service_server {
                 inner,
                 accept_compression_encodings: self.accept_compression_encodings,
                 send_compression_encodings: self.send_compression_encodings,
+                max_decoding_message_size: self.max_decoding_message_size,
+                max_encoding_message_size: self.max_encoding_message_size,
             }
         }
     }
     impl<T: TraceService> Clone for _Inner<T> {
         fn clone(&self) -> Self {
-            Self(self.0.clone())
+            Self(Arc::clone(&self.0))
         }
     }
     impl<T: std::fmt::Debug> std::fmt::Debug for _Inner<T> {
--- quickwit/quickwit-proto/src/quickwit_indexing_api.rs
+++ quickwit/quickwit-proto/src/quickwit_indexing_api.rs
@@ -34,7 +34,7 @@ pub mod indexing_service_client {
         /// Attempt to create a new client by connecting to a given endpoint.
         pub async fn connect<D>(dst: D) -> Result<Self, tonic::transport::Error>
         where
-            D: std::convert::TryInto<tonic::transport::Endpoint>,
+            D: TryInto<tonic::transport::Endpoint>,
             D::Error: Into<StdError>,
         {
             let conn = tonic::transport::Endpoint::new(dst)?.connect().await?;
@@ -90,11 +90,30 @@ pub mod indexing_service_client {
             self.inner = self.inner.accept_compressed(encoding);
             self
         }
+        /// Limits the maximum size of a decoded message.
+        ///
+        /// Default: `4MB`
+        #[must_use]
+        pub fn max_decoding_message_size(mut self, limit: usize) -> Self {
+            self.inner = self.inner.max_decoding_message_size(limit);
+            self
+        }
+        /// Limits the maximum size of an encoded message.
+        ///
+        /// Default: `usize::MAX`
+        #[must_use]
+        pub fn max_encoding_message_size(mut self, limit: usize) -> Self {
+            self.inner = self.inner.max_encoding_message_size(limit);
+            self
+        }
         /// / Apply an indexing plan on the node.
         pub async fn apply_indexing_plan(
             &mut self,
             request: impl tonic::IntoRequest<super::ApplyIndexingPlanRequest>,
-        ) -> Result<tonic::Response<super::ApplyIndexingPlanResponse>, tonic::Status> {
+        ) -> std::result::Result<
+            tonic::Response<super::ApplyIndexingPlanResponse>,
+            tonic::Status,
+        > {
             self.inner
                 .ready()
                 .await
@@ -108,7 +127,15 @@ pub mod indexing_service_client {
             let path = http::uri::PathAndQuery::from_static(
                 "/quickwit_indexing_api.IndexingService/applyIndexingPlan",
             );
-            self.inner.unary(request.into_request(), path, codec).await
+            let mut req = request.into_request();
+            req.extensions_mut()
+                .insert(
+                    GrpcMethod::new(
+                        "quickwit_indexing_api.IndexingService",
+                        "applyIndexingPlan",
+                    ),
+                );
+            self.inner.unary(req, path, codec).await
         }
     }
 }
@@ -123,13 +150,18 @@ pub mod indexing_service_server {
         async fn apply_indexing_plan(
             &self,
             request: tonic::Request<super::ApplyIndexingPlanRequest>,
-        ) -> Result<tonic::Response<super::ApplyIndexingPlanResponse>, tonic::Status>;
+        ) -> std::result::Result<
+            tonic::Response<super::ApplyIndexingPlanResponse>,
+            tonic::Status,
+        >;
     }
     #[derive(Debug)]
     pub struct IndexingServiceServer<T: IndexingService> {
         inner: _Inner<T>,
         accept_compression_encodings: EnabledCompressionEncodings,
         send_compression_encodings: EnabledCompressionEncodings,
+        max_decoding_message_size: Option<usize>,
+        max_encoding_message_size: Option<usize>,
     }
     struct _Inner<T>(Arc<T>);
     impl<T: IndexingService> IndexingServiceServer<T> {
@@ -142,6 +174,8 @@ pub mod indexing_service_server {
                 inner,
                 accept_compression_encodings: Default::default(),
                 send_compression_encodings: Default::default(),
+                max_decoding_message_size: None,
+                max_encoding_message_size: None,
             }
         }
         pub fn with_interceptor<F>(
@@ -165,6 +199,22 @@ pub mod indexing_service_server {
             self.send_compression_encodings.enable(encoding);
             self
         }
+        /// Limits the maximum size of a decoded message.
+        ///
+        /// Default: `4MB`
+        #[must_use]
+        pub fn max_decoding_message_size(mut self, limit: usize) -> Self {
+            self.max_decoding_message_size = Some(limit);
+            self
+        }
+        /// Limits the maximum size of an encoded message.
+        ///
+        /// Default: `usize::MAX`
+        #[must_use]
+        pub fn max_encoding_message_size(mut self, limit: usize) -> Self {
+            self.max_encoding_message_size = Some(limit);
+            self
+        }
     }
     impl<T, B> tonic::codegen::Service<http::Request<B>> for IndexingServiceServer<T>
     where
@@ -178,7 +228,7 @@ pub mod indexing_service_server {
         fn poll_ready(
             &mut self,
             _cx: &mut Context<'_>,
-        ) -> Poll<Result<(), Self::Error>> {
+        ) -> Poll<std::result::Result<(), Self::Error>> {
             Poll::Ready(Ok(()))
         }
         fn call(&mut self, req: http::Request<B>) -> Self::Future {
@@ -200,7 +250,7 @@ pub mod indexing_service_server {
                             &mut self,
                             request: tonic::Request<super::ApplyIndexingPlanRequest>,
                         ) -> Self::Future {
-                            let inner = self.0.clone();
+                            let inner = Arc::clone(&self.0);
                             let fut = async move {
                                 (*inner).apply_indexing_plan(request).await
                             };
@@ -209,6 +259,8 @@ pub mod indexing_service_server {
                     }
                     let accept_compression_encodings = self.accept_compression_encodings;
                     let send_compression_encodings = self.send_compression_encodings;
+                    let max_decoding_message_size = self.max_decoding_message_size;
+                    let max_encoding_message_size = self.max_encoding_message_size;
                     let inner = self.inner.clone();
                     let fut = async move {
                         let inner = inner.0;
@@ -218,6 +270,10 @@ pub mod indexing_service_server {
                             .apply_compression_config(
                                 accept_compression_encodings,
                                 send_compression_encodings,
+                            )
+                            .apply_max_message_size_config(
+                                max_decoding_message_size,
+                                max_encoding_message_size,
                             );
                         let res = grpc.unary(method, req).await;
                         Ok(res)
@@ -246,12 +302,14 @@ pub mod indexing_service_server {
                 inner,
                 accept_compression_encodings: self.accept_compression_encodings,
                 send_compression_encodings: self.send_compression_encodings,
+                max_decoding_message_size: self.max_decoding_message_size,
+                max_encoding_message_size: self.max_encoding_message_size,
             }
         }
     }
     impl<T: IndexingService> Clone for _Inner<T> {
         fn clone(&self) -> Self {
-            Self(self.0.clone())
+            Self(Arc::clone(&self.0))
         }
     }
     impl<T: std::fmt::Debug> std::fmt::Debug for _Inner<T> {
