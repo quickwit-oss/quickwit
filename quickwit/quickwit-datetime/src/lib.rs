404315f45f17315607a00074249e36071af2f0a4
Paul Masurel <paul@quickwit.io>
2023-05-26 15:27:52 +0900

    First stab: isolating the datetime in a new crate to use it in query-ast

---
+++ quickwit/quickwit-datetime/src/lib.rs
@@ -0,0 +1,6 @@
+mod date_time_format;
+mod date_time_parsing;
+
+pub use date_time_format::{DateTimeInputFormat, DateTimeOutputFormat, StrptimeParser};
+pub use date_time_parsing::{parse_date_time_int, parse_date_time_str};
+pub use tantivy::DateTime as TantivyDateTime;
