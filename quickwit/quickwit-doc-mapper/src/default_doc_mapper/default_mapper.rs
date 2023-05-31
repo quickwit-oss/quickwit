25fcb0d6d9416689bc4fde3e587ec6d0a0816780
Adrien Guillo <adrien.guillo@gmail.com>
2023-05-31 08:38:21 -0400

    Fix fmt

--- quickwit/quickwit-doc-mapper/src/default_doc_mapper/default_mapper.rs
+++ quickwit/quickwit-doc-mapper/src/default_doc_mapper/default_mapper.rs
@@ -173,16 +173,19 @@ impl TryFrom<DefaultDocMapperBuilder> for DefaultDocMapper {
         let mut default_search_field_names = Vec::new();
         for default_search_field_name in &builder.default_search_fields {
             if default_search_field_names.contains(default_search_field_name) {
-                bail!("Duplicated default search field: `{}`", default_search_field_name)
+                bail!(
+                    "Duplicated default search field: `{}`",
+                    default_search_field_name
+                )
             }
             let dynamic_field = schema.get_field(DYNAMIC_FIELD_NAME).ok();
             let (default_search_field, _json_path) = schema
                 .find_field_with_default(default_search_field_name, dynamic_field)
-                .with_context(|| format!("Unknown default search field: `{default_search_field_name}`"))?;
+                .with_context(|| {
+                    format!("Unknown default search field: `{default_search_field_name}`")
+                })?;
             if !schema.get_field_entry(default_search_field).is_indexed() {
-                bail!(
-                    "Default search field `{default_search_field_name}` is not indexed.",
-                );
+                bail!("Default search field `{default_search_field_name}` is not indexed.",);
             }
             default_search_field_names.push(default_search_field_name.clone());
         }
