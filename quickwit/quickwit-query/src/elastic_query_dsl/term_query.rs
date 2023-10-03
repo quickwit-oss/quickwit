91540c6c63a513850ccfb7d662a93d215e565ecf
Paul Masurel <paul@quickwit.io>
2023-10-03 18:22:39 +0900

    Refactor the string_or_struct logic and enables it for TermQuery. (#3906)
    * Refactor the string_or_struct logic and enables it for TermQuery.
    
    Closes #3900
    
    * Allowing integers in term queries.

--- quickwit/quickwit-query/src/elastic_query_dsl/term_query.rs
+++ quickwit/quickwit-query/src/elastic_query_dsl/term_query.rs
@@ -17,18 +17,61 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-use serde::Deserialize;
+use serde::{Deserialize, Deserializer, Serialize};
 
+use super::StringOrStructForSerialization;
 use crate::elastic_query_dsl::one_field_map::OneFieldMap;
 use crate::elastic_query_dsl::{ConvertableToQueryAst, ElasticQueryDslInner};
 use crate::not_nan_f32::NotNaNf32;
 use crate::query_ast::{self, QueryAst};
 
-pub type TermQuery = OneFieldMap<TermQueryValue>;
+#[derive(Deserialize, Debug, PartialEq, Eq, Clone)]
+#[serde(from = "OneFieldMap<StringOrStructForSerialization<TermQueryParams>>")]
+pub struct TermQuery {
+    pub field: String,
+    pub value: TermQueryParams,
+}
 
-#[derive(PartialEq, Eq, Debug, Deserialize, Clone)]
+impl From<OneFieldMap<StringOrStructForSerialization<TermQueryParams>>> for TermQuery {
+    fn from(one_field_map: OneFieldMap<StringOrStructForSerialization<TermQueryParams>>) -> Self {
+        TermQuery {
+            field: one_field_map.field,
+            value: one_field_map.value.inner,
+        }
+    }
+}
+
+impl From<String> for TermQueryParams {
+    fn from(query: String) -> TermQueryParams {
+        TermQueryParams {
+            value: query,
+            boost: None,
+        }
+    }
+}
+
+#[derive(Deserialize)]
+#[serde(untagged)]
+enum TermValue {
+    I64(i64),
+    U64(u64),
+    Str(String),
+}
+
+fn deserialize_term_value<'de, D>(deserializer: D) -> Result<String, D::Error>
+where D: Deserializer<'de> {
+    let term_value = TermValue::deserialize(deserializer)?;
+    match term_value {
+        TermValue::I64(i64) => Ok(i64.to_string()),
+        TermValue::U64(u64) => Ok(u64.to_string()),
+        TermValue::Str(str) => Ok(str),
+    }
+}
+
+#[derive(PartialEq, Eq, Debug, Serialize, Deserialize, Clone)]
 #[serde(deny_unknown_fields)]
-pub struct TermQueryValue {
+pub struct TermQueryParams {
+    #[serde(deserialize_with = "deserialize_term_value")]
     pub value: String,
     #[serde(default)]
     pub boost: Option<NotNaNf32>,
@@ -37,7 +80,7 @@ pub struct TermQueryValue {
 pub fn term_query_from_field_value(field: impl ToString, value: impl ToString) -> TermQuery {
     TermQuery {
         field: field.to_string(),
-        value: TermQueryValue {
+        value: TermQueryParams {
             value: value.to_string(),
             boost: None,
         },
@@ -52,7 +95,7 @@ impl From<TermQuery> for ElasticQueryDslInner {
 
 impl ConvertableToQueryAst for TermQuery {
     fn convert_to_query_ast(self) -> anyhow::Result<QueryAst> {
-        let TermQueryValue { value, boost } = self.value;
+        let TermQueryParams { value, boost } = self.value;
         let term_ast: QueryAst = query_ast::TermQuery {
             field: self.field,
             value,
@@ -70,6 +113,20 @@ mod tests {
     fn test_term_query_simple() {
         let term_query_json = r#"{ "product_id": { "value": "61809" } }"#;
         let term_query: TermQuery = serde_json::from_str(term_query_json).unwrap();
+        assert_eq!(
+            &term_query,
+            &term_query_from_field_value("product_id", "61809")
+        );
+    }
+
+    #[test]
+    fn test_term_query_deserialization_in_short_format() {
+        let term_query: TermQuery = serde_json::from_str(
+            r#"{
+            "product_id": "61809"
+        }"#,
+        )
+        .unwrap();
         assert_eq!(
             &term_query,
             &term_query_from_field_value("product_id", "61809")
