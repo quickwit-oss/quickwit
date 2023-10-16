f65e68a1dbac64b87197e5487a3b64c24cc2ff55
Kamalesh Palanisamy <kamalesh800@gmail.com>
2023-10-15 22:39:49 -0400

    Update sql strings with sea_query sql statements

--- quickwit/quickwit-metastore/src/metastore/postgresql_model.rs
+++ quickwit/quickwit-metastore/src/metastore/postgresql_model.rs
@@ -22,6 +22,7 @@ use std::str::FromStr;
 
 use quickwit_proto::metastore::{DeleteQuery, DeleteTask, MetastoreError, MetastoreResult};
 use quickwit_proto::types::IndexUid;
+use sea_query::{Iden, Write};
 use tracing::error;
 
 use crate::{IndexMetadata, Split, SplitMetadata, SplitState};
@@ -59,6 +60,31 @@ impl PgIndex {
         // values upon deserialization.
         index_metadata.create_timestamp = self.create_timestamp.assume_utc().unix_timestamp();
         Ok(index_metadata)
+    }
+}
+
+#[derive(Iden, Clone, Copy)]
+#[allow(dead_code)]
+pub enum Splits {
+    Table,
+    SplitState,
+    TimeRangeStart,
+    TimeRangeEnd,
+    CreateTimestamp,
+    UpdateTimestamp,
+    PublishTimestamp,
+    MaturityTimestamp,
+    Tags,
+    SplitMetadataJson,
+    IndexUid,
+    DeleteOpstamp,
+}
+
+pub struct ToTimestampFunc;
+
+impl Iden for ToTimestampFunc {
+    fn unquoted(&self, s: &mut dyn Write) {
+        write!(s, "TO_TIMESTAMP").unwrap()
     }
 }
 
