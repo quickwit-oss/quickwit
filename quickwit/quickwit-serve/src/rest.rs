d0e9e8a5a466e235edc66568b33abe9542aed76a
Eugene Tolbakov <ev.tolbakov@gmail.com>
2023-10-30 21:15:59 +0000

    fix: add zstd

--- quickwit/quickwit-serve/src/rest.rs
+++ quickwit/quickwit-serve/src/rest.rs
@@ -153,7 +153,7 @@ pub(crate) async fn start_rest_server(
                 .compress_when(compression_predicate),
         )
         .layer(cors)
-        .layer(DecompressionLayer::new().gzip(true))
+        .layer(DecompressionLayer::new().gzip(true).zstd(true))
         .service(warp_service);
 
     info!(
