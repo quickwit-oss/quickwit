1e7aef0e983d3f8442ecc09018cfd20e97960ffc
trinity-1686a <trinity@quickwit.io>
2023-10-31 15:09:59 +0100

    add license headers

--- quickwit/quickwit-query/src/query_ast/wildcard_query.rs
+++ quickwit/quickwit-query/src/query_ast/wildcard_query.rs
@@ -16,6 +16,7 @@
 //
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 use anyhow::Context;
 use serde::{Deserialize, Serialize};
 use tantivy::json_utils::JsonTermWriter;
