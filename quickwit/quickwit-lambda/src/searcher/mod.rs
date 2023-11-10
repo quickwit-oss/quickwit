669094fd72da882d179dcc0564ef928a295298a7
Remi Dettai <rdettai@gmail.com>
2023-11-10 10:29:49 +0000

    Cache and better tracing

--- quickwit/quickwit-lambda/src/searcher/mod.rs
+++ quickwit/quickwit-lambda/src/searcher/mod.rs
@@ -17,6 +17,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+mod environment;
 mod handler;
 mod search;
 
