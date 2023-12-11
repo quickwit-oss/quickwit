9f20660e862dc4a675a7b95962b29a8f6b8f81e8
Paul Masurel <paul@quickwit.io>
2023-12-11 20:13:11 +0900

    Remove two bugs linked to the last mailbox in the actor framework (#4251)
    The weak mailbox was messing the refcounting.
    and we technically had a race condition.
    
    The bug could surfaced on any actor looping alone,
    like the control plane.
    
    Closes #4248

--- quickwit/quickwit-actors/src/mailbox.rs
+++ quickwit/quickwit-actors/src/mailbox.rs
@@ -24,17 +24,13 @@ use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::{Arc, Weak};
 use std::time::Instant;
 
-use async_trait::async_trait;
 use quickwit_common::metrics::IntCounter;
 use tokio::sync::oneshot;
 
 use crate::channel_with_priority::{Receiver, Sender, TrySendError};
 use crate::envelope::{wrap_in_envelope, Envelope};
 use crate::scheduler::SchedulerClient;
-use crate::{
-    Actor, ActorContext, ActorExitStatus, AskError, DeferableReplyHandler, Handler, QueueCapacity,
-    RecvError, SendError,
-};
+use crate::{Actor, AskError, Command, DeferableReplyHandler, QueueCapacity, RecvError, SendError};
 
 /// A mailbox is the object that makes it possible to send a message
 /// to an actor.
@@ -76,37 +72,8 @@ impl<A: Actor> Drop for Mailbox<A> {
             // This was the last mailbox.
             // `ref_count == 1` means that only the mailbox in the ActorContext
             // is remaining.
-            let _ = self.send_message_with_high_priority(LastMailbox);
+            let _ = self.send_message_with_high_priority(Command::Nudge);
         }
-    }
-}
-
-#[derive(Debug)]
-struct LastMailbox;
-
-#[async_trait]
-impl<A: Actor> Handler<LastMailbox> for A {
-    type Reply = ();
-
-    async fn handle(
-        &mut self,
-        _: LastMailbox,
-        _ctx: &ActorContext<Self>,
-    ) -> Result<(), ActorExitStatus> {
-        // Being the last mailbox does not necessarily mean that we
-        // want to stop the processing.
-        //
-        // There could be pending message in the queue that will
-        // spawn actors which will get a new copy of the mailbox
-        // etc.
-        //
-        // For that reason, the logic that really detects
-        // the last mailbox happens when all message have been drained.
-        //
-        // The `LastMailbox` message is just here to make sure the actor
-        // loop does not get stuck waiting for a message that does
-        // will never come.
-        Ok(())
     }
 }
 
@@ -451,6 +418,7 @@ impl<A: Actor> WeakMailbox<A> {
     pub fn upgrade(&self) -> Option<Mailbox<A>> {
         let inner = self.inner.upgrade()?;
         let ref_count = self.ref_count.upgrade()?;
+        ref_count.fetch_add(1, Ordering::SeqCst);
         Some(Mailbox { inner, ref_count })
     }
 }
@@ -462,7 +430,7 @@ mod tests {
 
     use super::*;
     use crate::tests::{Ping, PingReceiverActor};
-    use crate::Universe;
+    use crate::{ActorContext, ActorExitStatus, Handler, Universe};
 
     #[tokio::test]
     async fn test_weak_mailbox_downgrade_upgrade() {
@@ -622,5 +590,18 @@ mod tests {
             mailbox.try_send_message(Ping).unwrap_err(),
             TrySendError::Disconnected
         ));
+    }
+
+    #[tokio::test]
+    async fn test_weak_mailbox_ref_count() {
+        let universe = Universe::with_accelerated_time();
+        let (mailbox, _inbox) = universe
+            .create_mailbox::<PingReceiverActor>("hello".to_string(), QueueCapacity::Bounded(1));
+        assert!(mailbox.is_last_mailbox());
+        let weak_mailbox = mailbox.downgrade();
+        let second_mailbox = weak_mailbox.upgrade().unwrap();
+        assert!(!mailbox.is_last_mailbox());
+        drop(second_mailbox);
+        assert!(mailbox.is_last_mailbox());
     }
 }
