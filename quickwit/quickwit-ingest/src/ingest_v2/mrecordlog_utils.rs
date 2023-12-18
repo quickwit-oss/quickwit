0d19610d0b72ea46b184178396ad95abc61cca67
Adrien Guillo <adrien.guillo@gmail.com>
2023-12-18 12:09:41 +0100

    Close shards on IO error

--- quickwit/quickwit-ingest/src/ingest_v2/mrecordlog_utils.rs
+++ quickwit/quickwit-ingest/src/ingest_v2/mrecordlog_utils.rs
@@ -18,21 +18,68 @@
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 use std::io;
+use std::iter::once;
 use std::ops::RangeInclusive;
 
 use bytesize::ByteSize;
-use mrecordlog::error::DeleteQueueError;
+use mrecordlog::error::{AppendError, DeleteQueueError};
 use mrecordlog::MultiRecordLog;
-use quickwit_proto::types::QueueId;
+use quickwit_proto::ingest::DocBatchV2;
+use quickwit_proto::types::{Position, QueueId};
+
+use crate::MRecord;
+
+#[derive(Debug, thiserror::Error)]
+pub(super) enum AppendDocBatchError {
+    #[error("IO error: {0}")]
+    Io(#[from] io::Error),
+    #[error("WAL queue `{0}` not found")]
+    QueueNotFound(QueueId),
+}
+
+/// Appends a non-empty document batch to the WAL queue `queue_id`.
+///
+/// # Panics
+///
+/// Panics if `doc_batch` is empty.
+pub(super) async fn append_non_empty_doc_batch(
+    mrecordlog: &mut MultiRecordLog,
+    queue_id: &QueueId,
+    doc_batch: &DocBatchV2,
+    force_commit: bool,
+) -> Result<Position, AppendDocBatchError> {
+    let append_result = if force_commit {
+        let encoded_mrecords = doc_batch
+            .docs()
+            .map(|doc| MRecord::Doc(doc).encode())
+            .chain(once(MRecord::Commit.encode()));
+        mrecordlog
+            .append_records(queue_id, None, encoded_mrecords)
+            .await
+    } else {
+        let encoded_mrecords = doc_batch.docs().map(|doc| MRecord::Doc(doc).encode());
+        mrecordlog
+            .append_records(queue_id, None, encoded_mrecords)
+            .await
+    };
+    match append_result {
+        Ok(Some(offset)) => Ok(Position::offset(offset)),
+        Ok(None) => panic!("`doc_batch` should not be empty"),
+        Err(AppendError::IoError(io_error)) => Err(AppendDocBatchError::Io(io_error)),
+        Err(AppendError::MissingQueue(queue_id)) => {
+            Err(AppendDocBatchError::QueueNotFound(queue_id))
+        }
+        Err(AppendError::Past) => {
+            panic!("`append_records` should be called with `position_opt: None`")
+        }
+    }
+}
 
 #[derive(Debug, Clone, Copy)]
 pub(super) struct MRecordLogUsage {
     pub disk: ByteSize,
     pub memory: ByteSize,
 }
-
-#[derive(Debug, Clone, Copy)]
-pub(super) struct MemoryUsage(ByteSize);
 
 /// Error returned when the mrecordlog does not have enough capacity to store some records.
 #[derive(Debug, Clone, Copy, thiserror::Error)]
@@ -122,6 +169,37 @@ pub(super) fn queue_position_range(
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[tokio::test]
+    async fn test_append_non_empty_doc_batch() {
+        let tempdir = tempfile::tempdir().unwrap();
+        let mut mrecordlog = MultiRecordLog::open(tempdir.path()).await.unwrap();
+
+        let queue_id = "test-queue".to_string();
+        let doc_batch = DocBatchV2::for_test(["test-doc-foo"]);
+
+        let append_error =
+            append_non_empty_doc_batch(&mut mrecordlog, &queue_id, &doc_batch, false)
+                .await
+                .unwrap_err();
+
+        assert!(matches!(
+            append_error,
+            AppendDocBatchError::QueueNotFound(..)
+        ));
+
+        mrecordlog.create_queue(&queue_id).await.unwrap();
+
+        let position = append_non_empty_doc_batch(&mut mrecordlog, &queue_id, &doc_batch, false)
+            .await
+            .unwrap();
+        assert_eq!(position, Position::offset(0u64));
+
+        let position = append_non_empty_doc_batch(&mut mrecordlog, &queue_id, &doc_batch, true)
+            .await
+            .unwrap();
+        assert_eq!(position, Position::offset(2u64));
+    }
 
     #[tokio::test]
     async fn test_check_enough_capacity() {
