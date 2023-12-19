c2a2e771f6d9222708e4fffc7af439dd86d9740c
Paul Masurel <paul@quickwit.io>
2023-12-19 15:16:43 +0900

    debug info

--- quickwit/quickwit-control-plane/src/control_plane.rs
+++ quickwit/quickwit-control-plane/src/control_plane.rs
@@ -225,6 +225,7 @@ impl Handler<ControlPlanLoop> for ControlPlane {
         _message: ControlPlanLoop,
         ctx: &ActorContext<Self>,
     ) -> Result<(), ActorExitStatus> {
+        self.indexing_scheduler.schedule_indexing_plan_if_needed(&self.model);
         self.indexing_scheduler.control_running_plan(&self.model);
         ctx.schedule_self_msg(CONTROL_PLAN_LOOP_INTERVAL, ControlPlanLoop)
             .await;
