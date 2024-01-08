a9c063c2526a18bea939dc32b88444300b19bc28
Remi Dettai <rdettai@gmail.com>
2024-01-08 10:08:44 +0000

    Fork the CLI local search and index methods

--- quickwit/quickwit-lambda/src/bin/searcher.rs
+++ quickwit/quickwit-lambda/src/bin/searcher.rs
@@ -17,13 +17,41 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-use lambda_runtime::service_fn;
-use quickwit_lambda::{search_handler, setup_lambda_tracer};
+use lambda_runtime::{service_fn, Error, LambdaEvent};
+use quickwit_lambda::{search, setup_lambda_tracer, SearchArgs};
+use serde_json::Value;
+use tracing::{debug, error};
+
+pub async fn handler(_event: LambdaEvent<Value>) -> Result<Value, Error> {
+    let ingest_res = search(SearchArgs {
+        index_id: String::from("hdfs-logs"),
+        query: String::new(),
+        aggregation: None,
+        max_hits: 10,
+        start_offset: 0,
+        search_fields: None,
+        snippet_fields: None,
+        start_timestamp: None,
+        end_timestamp: None,
+        sort_by_field: None,
+    })
+    .await;
+    match ingest_res {
+        Ok(resp) => {
+            debug!(resp=?resp, "Search succeeded");
+            Ok(serde_json::to_value(resp)?)
+        }
+        Err(e) => {
+            error!(err=?e, "Search failed");
+            return Err(anyhow::anyhow!("Query failed").into());
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     setup_lambda_tracer()?;
-    let func = service_fn(search_handler);
+    let func = service_fn(handler);
     lambda_runtime::run(func)
         .await
         .map_err(|e| anyhow::anyhow!(e))
