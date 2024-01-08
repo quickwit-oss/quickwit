7617c89342ec54e10189a4039901045afac25101
Remi Dettai <rdettai@gmail.com>
2024-01-08 10:09:32 +0000

    wip: trying to setup tracing

--- quickwit/quickwit-lambda/src/bin/searcher.rs
+++ quickwit/quickwit-lambda/src/bin/searcher.rs
@@ -18,12 +18,12 @@
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 use lambda_runtime::service_fn;
+use quickwit_lambda::logger;
 use quickwit_lambda::searcher::handler;
-use quickwit_lambda::setup_lambda_tracer;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    setup_lambda_tracer()?;
+    logger::setup_lambda_tracer()?;
     let func = service_fn(handler);
     lambda_runtime::run(func)
         .await
