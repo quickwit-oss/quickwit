376df7194d1e945bd6b21933772df505243b9520
PSeitz <PSeitz@users.noreply.github.com>
2024-01-25 02:02:37 +0100

    fix agg result on empty index (#4449)
    * fix agg result on empty index
    
    empty indices return empty intermediate aggregation results. To get the
    correct result structure we need to call `into_final` instead of early exit.
    
    fixes #4437
    
    * clear aggregation if no index is hit

--- quickwit/quickwit-search/src/root.rs
+++ quickwit/quickwit-search/src/root.rs
@@ -876,11 +876,15 @@ async fn root_search_aux(
     )
     .await?;
 
-    let aggregation_result_json_opt = finalize_aggregation_if_any(
+    let mut aggregation_result_json_opt = finalize_aggregation_if_any(
         &search_request,
         first_phase_result.intermediate_aggregation_result,
         searcher_context,
     )?;
+    // In case there is no index, we don't want the response to contain any aggregation structure
+    if indexes_metas_for_leaf_search.is_empty() {
+        aggregation_result_json_opt = None;
+    }
 
     Ok(SearchResponse {
         aggregation: aggregation_result_json_opt,
@@ -895,25 +899,39 @@ async fn root_search_aux(
 }
 
 fn finalize_aggregation(
-    intermediate_aggregation_result_bytes: &[u8],
+    intermediate_aggregation_result_bytes_opt: Option<Vec<u8>>,
     aggregations: QuickwitAggregations,
     searcher_context: &SearcherContext,
-) -> crate::Result<String> {
+) -> crate::Result<Option<String>> {
     let merge_aggregation_result = match aggregations {
         QuickwitAggregations::FindTraceIdsAggregation(_) => {
+            let Some(intermediate_aggregation_result_bytes) =
+                intermediate_aggregation_result_bytes_opt
+            else {
+                return Ok(None);
+            };
             // The merge collector has already merged the intermediate results.
-            let aggs: Vec<Span> = postcard::from_bytes(intermediate_aggregation_result_bytes)?;
+            let aggs: Vec<Span> = postcard::from_bytes(&intermediate_aggregation_result_bytes)?;
             serde_json::to_string(&aggs)?
         }
         QuickwitAggregations::TantivyAggregations(aggregations) => {
-            let intermediate_aggregation_results: IntermediateAggregationResults =
-                postcard::from_bytes(intermediate_aggregation_result_bytes)?;
+            let intermediate_aggregation_results =
+                if let Some(intermediate_aggregation_result_bytes) =
+                    intermediate_aggregation_result_bytes_opt
+                {
+                    let intermediate_aggregation_results: IntermediateAggregationResults =
+                        postcard::from_bytes(&intermediate_aggregation_result_bytes)?;
+                    intermediate_aggregation_results
+                } else {
+                    // Default, to return correct structure
+                    Default::default()
+                };
             let final_aggregation_results: AggregationResults = intermediate_aggregation_results
                 .into_final_result(aggregations, &searcher_context.get_aggregation_limits())?;
             serde_json::to_string(&final_aggregation_results)?
         }
     };
-    Ok(merge_aggregation_result)
+    Ok(Some(merge_aggregation_result))
 }
 
 fn finalize_aggregation_if_any(
@@ -925,15 +943,12 @@ fn finalize_aggregation_if_any(
         return Ok(None);
     };
     let aggregations: QuickwitAggregations = serde_json::from_str(aggregations_json)?;
-    let Some(intermediate_result_bytes) = intermediate_aggregation_result_bytes_opt else {
-        return Ok(None);
-    };
     let aggregation_result_json = finalize_aggregation(
-        &intermediate_result_bytes[..],
+        intermediate_aggregation_result_bytes_opt,
         aggregations,
         searcher_context,
     )?;
-    Ok(Some(aggregation_result_json))
+    Ok(aggregation_result_json)
 }
 
 /// Checks that all of the index researched as found.
