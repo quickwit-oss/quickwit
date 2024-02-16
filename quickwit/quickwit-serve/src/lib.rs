9975e0eb3ade256df2e556b4ea14bfac40da6f4b
Adrien Guillo <adrien.guillo@gmail.com>
2024-02-16 13:52:58 -0500

    Wait for metastore service for 5 minutes

--- quickwit/quickwit-serve/src/lib.rs
+++ quickwit/quickwit-serve/src/lib.rs
@@ -353,9 +353,9 @@ pub async fn serve_quickwit(
         if let Some(metastore_server) = &metastore_server_opt {
             metastore_server.clone()
         } else {
-            // Wait for a metastore service to be available for at most 60 seconds.
+            // Wait for a metastore service to be available for at most 5 minutes.
             if cluster
-                .wait_for_ready_members(has_node_with_metastore_service, Duration::from_secs(60))
+                .wait_for_ready_members(has_node_with_metastore_service, Duration::from_secs(300))
                 .await
                 .is_err()
             {
