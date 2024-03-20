13dfc0d176c805b859d2883b3e978364f8286f91
Remi Dettai <rdettai@gmail.com>
2024-03-20 10:54:25 +0000

    Add terraform module for ECS

---
+++ distribution/ecs/quickwit/service/ecs.tf
@@ -0,0 +1,123 @@
+module "quickwit_service" {
+  source  = "terraform-aws-modules/ecs/aws//modules/service"
+  version = "5.9.3"
+
+  name        = "quickwit-${var.service_name}-${var.module_id}"
+  cluster_arn = var.cluster_arn
+
+  cpu    = var.service_config.cpu
+  memory = var.service_config.memory
+
+  container_definitions = merge(var.sidecar_container_definitions, {
+    quickwit = {
+      cpu    = var.service_config.cpu
+      memory = var.service_config.memory
+
+      essential                 = true
+      image                     = var.quickwit_image
+      enable_cloudwatch_logging = var.enable_cloudwatch_logging
+
+      command = ["run"]
+
+      environment = concat(local.quickwit_common_environment, [
+        {
+          name  = "QW_ENABLED_SERVICES"
+          value = var.service_name
+        }
+      ])
+
+      secrets = [
+        {
+          name      = "QW_METASTORE_URI"
+          valueFrom = var.postgres_credential_arn
+        }
+      ]
+
+      port_mappings = [
+        {
+          name          = "rest"
+          containerPort = 7280
+          protocol      = "tcp"
+        },
+        {
+          name          = "grpc"
+          containerPort = 7281
+          protocol      = "tcp"
+        },
+        {
+          name          = "gossip"
+          containerPort = 7280
+          protocol      = "udp"
+        }
+      ]
+
+      log_configuration = var.log_configuration
+
+      mount_points = [
+        {
+          sourceVolume  = "quickwit-data-vol"
+          containerPath = local.quickwit_data_dir
+        }
+      ]
+
+      dependencies = var.sidecar_container_dependencies
+    }
+  })
+
+  requires_compatibilities = ["FARGATE"]
+  runtime_platform = {
+    operating_system_family = "LINUX"
+    cpu_architecture        = var.quickwit_cpu_architecture
+  }
+
+  service_registries = {
+    registry_arn   = var.service_discovery_registry_arn
+    container_name = "quickwit"
+  }
+
+  subnet_ids = var.subnet_ids
+  security_group_rules = {
+    ingress_internal = {
+      type      = "ingress"
+      from_port = 7280
+      to_port   = 7281
+      protocol  = "-1"
+
+      source_security_group_id = var.quickwit_cluster_member_sg_id
+    }
+    ingress_external = {
+      type      = "ingress"
+      from_port = 7280
+      to_port   = 7281
+      protocol  = "-1"
+
+      cidr_blocks = var.ingress_cidr_blocks
+    }
+    egress_all = {
+      type      = "egress"
+      from_port = 0
+      to_port   = 0
+      protocol  = "-1"
+
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+  security_group_ids = [var.quickwit_cluster_member_sg_id]
+
+  enable_autoscaling = false
+  desired_count      = var.service_config.desired_count
+
+  volume = [
+    {
+      name = "quickwit-data-vol"
+    }
+  ]
+
+  task_exec_ssm_param_arns = [
+    var.postgres_credential_arn
+  ]
+
+  tasks_iam_role_policies = {
+    s3_access = var.s3_access_policy_arn
+  }
+}
---
+++ distribution/ecs/quickwit/service/variables.tf
@@ -0,0 +1,57 @@
+variable "service_name" {
+  description = "One of indexer, metastore, searcher, control_plane, janitor"
+}
+
+variable "service_discovery_registry_arn" {}
+
+variable "sidecar_container_definitions" {}
+
+variable "sidecar_container_dependencies" {
+  type = list(object({
+    containerName = string
+    condition     = string
+  }))
+  default = []
+}
+
+variable "log_configuration" {}
+
+variable "enable_cloudwatch_logging" {
+  type = bool
+}
+
+variable "cluster_arn" {}
+
+variable "ingress_cidr_blocks" {
+  type = list(string)
+}
+
+variable "quickwit_cluster_member_sg_id" {}
+
+variable "subnet_ids" {
+  type = list(string)
+}
+
+variable "postgres_credential_arn" {}
+
+variable "quickwit_image" {}
+
+variable "service_config" {
+  type = object({
+    desired_count = optional(number, 1)
+    memory        = number
+    cpu           = number
+  })
+}
+
+variable "quickwit_index_s3_prefix" {}
+
+variable "quickwit_peer_list" {
+  type = list(string)
+}
+
+variable "s3_access_policy_arn" {}
+
+variable "quickwit_cpu_architecture" {}
+
+variable "module_id" {}
