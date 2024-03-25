d916353d7a67639c657ecb6972e27ec186d57e18
Remi Dettai <rdettai@gmail.com>
2024-03-25 09:22:05 +0000

    Make module id customizable

--- distribution/ecs/quickwit/configs.tf
+++ distribution/ecs/quickwit/configs.tf
@@ -7,8 +7,9 @@ locals {
     "${aws_service_discovery_service.searcher.name}.${aws_service_discovery_private_dns_namespace.quickwit_internal.name}",
   ]
 
-  # a random id to avoid conflicts when deploying this module multiple times
-  module_id = random_id.module.hex
+  # id to avoid conflicts when deploying this module multiple times (random by default)
+  module_id = var.module_id == "" ? random_id.module.hex : var.module_id
+  s3_id     = var.module_id == "" ? random_id.module.hex : "${var.module_id}-${random_id.module.hex}"
 
   quickwit_index_s3_prefix = var.quickwit_index_s3_prefix == "" ? aws_s3_bucket.index[0].id : var.quickwit_index_s3_prefix
 }
