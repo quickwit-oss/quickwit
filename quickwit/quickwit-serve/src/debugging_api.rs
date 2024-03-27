11f4054cbdfcb5bd3a292cac3683e5cfddc07415
trinity-1686a <trinity@quickwit.io>
2024-03-27 15:26:17 +0100

    move debugging api endpoint

--- quickwit/quickwit-serve/src/debugging_api.rs
+++ quickwit/quickwit-serve/src/debugging_api.rs
@@ -17,12 +17,17 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+use std::sync::Arc;
+
 use quickwit_proto::control_plane::{
     ControlPlaneService, ControlPlaneServiceClient, GetDebugStateRequest,
 };
+use warp::{Filter, Rejection};
+
+use crate::{with_arg, QuickwitServices};
 
 #[derive(utoipa::OpenApi)]
-#[openapi(paths(debugging_handler))]
+#[openapi(paths(control_plane_debugging_handler))]
 /// Endpoints which are weirdly tied to another crate with no
 /// other bits of information attached.
 ///
@@ -30,10 +35,20 @@ use quickwit_proto::control_plane::{
 /// Then it should have it's own specific API group.
 pub struct DebugApi;
 
+pub(crate) fn debugging_routes(
+    quickwit_services: Arc<QuickwitServices>,
+) -> impl Filter<Extract = (impl warp::Reply,), Error = Rejection> + Clone {
+    let debugging_routes = warp::path!("api" / "debugging" / ..);
+
+    debugging_routes.and(control_plane_debugging_handler(
+        quickwit_services.control_plane_service.clone(),
+    ))
+}
+
 #[utoipa::path(
     get,
-    tag = "Get debug information for node",
-    path = "/",
+    tag = "Get debug informations about the control plane from the node viewpoint",
+    path = "/shard",
     responses(
         (status = 200, description = "Successfully fetched debugging info.", body = GetDebugStateRequestResponse),
     ),
@@ -42,16 +57,23 @@ pub struct DebugApi;
 ///
 /// The format is not guaranteed to ever be stable, and is meant to provide some introspection to
 /// help with debugging.
-pub async fn debugging_handler(
-    mut control_plane_service_client: ControlPlaneServiceClient,
-) -> impl warp::Reply {
-    let debug_info = control_plane_service_client
-        .get_debug_state(GetDebugStateRequest {})
-        .await;
-    crate::rest_api_response::RestApiResponse::new(
-        &debug_info,
-        // TODO error code on error
-        hyper::StatusCode::OK,
-        crate::format::BodyFormat::PrettyJson,
-    )
+pub fn control_plane_debugging_handler(
+    control_plane_service_client: ControlPlaneServiceClient,
+) -> impl Filter<Extract = (impl warp::Reply,), Error = Rejection> + Clone {
+    warp::path("shard")
+        .and(warp::path::end())
+        .and(with_arg(control_plane_service_client.clone()))
+        .then(
+            |mut control_plane_service_client: ControlPlaneServiceClient| async move {
+                let debug_info = control_plane_service_client
+                    .get_debug_state(GetDebugStateRequest {})
+                    .await;
+                crate::rest_api_response::RestApiResponse::new(
+                    &debug_info,
+                    // TODO error code on error
+                    hyper::StatusCode::OK,
+                    crate::format::BodyFormat::PrettyJson,
+                )
+            },
+        )
 }
