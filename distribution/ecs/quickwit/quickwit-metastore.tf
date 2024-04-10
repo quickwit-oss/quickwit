0bb20f37f58e7ca499f6fe59ca42da1e29e0129e
Remi Dettai <rdettai@gmail.com>
2024-04-10 09:46:20 +0200

    Add terraform module for ECS (#4765)
    * Add terraform module for ECS
    
    * Make module id customizable
    
    * More example and docs
    
    * Address review comments

---
+++ distribution/ecs/quickwit/quickwit-metastore.tf
@@ -0,0 +1,37 @@
+module "quickwit_metastore" {
+  source                         = "./service"
+  service_name                   = "metastore"
+  service_discovery_registry_arn = aws_service_discovery_service.metastore.arn
+  cluster_arn                    = module.ecs_cluster.arn
+  postgres_credential_arn        = aws_ssm_parameter.postgres_credential.arn
+  quickwit_peer_list             = local.quickwit_peer_list
+  s3_access_policy_arn           = aws_iam_policy.quickwit.arn
+  module_id                      = local.module_id
+  quickwit_cluster_member_sg_id  = aws_security_group.quickwit_cluster_member_sg.id
+
+  subnet_ids                     = var.subnet_ids
+  ingress_cidr_blocks            = var.quickwit_ingress_cidr_blocks
+  quickwit_image                 = var.quickwit_image
+  quickwit_cpu_architecture      = var.quickwit_cpu_architecture
+  sidecar_container_definitions  = var.sidecar_container_definitions
+  sidecar_container_dependencies = var.sidecar_container_dependencies
+  log_configuration              = var.log_configuration
+  enable_cloudwatch_logging      = var.enable_cloudwatch_logging
+  service_config                 = var.quickwit_metastore
+  quickwit_index_s3_prefix       = local.quickwit_index_s3_prefix
+}
+
+resource "aws_service_discovery_service" "metastore" {
+  name = "metastore"
+
+  dns_config {
+    namespace_id = aws_service_discovery_private_dns_namespace.quickwit_internal.id
+
+    dns_records {
+      ttl  = 10
+      type = "A"
+    }
+
+    routing_policy = "MULTIVALUE"
+  }
+}
