6283ac395a9e6707459b213aa80435d9a42a6dc1
trinity-1686a <trinity@quickwit.io>
2024-05-11 19:10:32 +0200

    show aggregation results

--- quickwit/quickwit-ui/src/utils/models.ts
+++ quickwit/quickwit-ui/src/utils/models.ts
@@ -48,7 +48,7 @@ export type Entry = {
 export const DATE_TIME_WITH_SECONDS_FORMAT = "YYYY/MM/DD HH:mm:ss";
 export const DATE_TIME_WITH_MILLISECONDS_FORMAT = "YYYY/MM/DD HH:mm:ss.SSS";
 
-// Returns a flatten array of fields and nested fields found in the given `FieldMapping` array. 
+// Returns a flatten array of fields and nested fields found in the given `FieldMapping` array.
 export function getAllFields(field_mappings: Array<FieldMapping>): Field[] {
   const fields: Field[] = [];
   for (const field_mapping of field_mappings) {
@@ -108,6 +108,78 @@ export type TermAgg = {
 
 export type HistogramAgg = {
   interval: string;
+}
+
+export type ParsedAggregationResult = TermResult | HistogramResult| null;
+
+export type TermResult = {term: string, value: number}[];
+
+export type HistogramResult = {
+  timestamps: Date[],
+  data: {name: string | undefined, value: number[]}[],
+}
+
+export function extractAggregationResults(aggregation: any): ParsedAggregationResult {
+  console.log("doing the conversion");
+  const extract_value = (entry: any) => {
+    if (Object.prototype.hasOwnProperty.call(entry, "metric")) {
+      return entry.metric.value || 0;
+    } else {
+      return entry.doc_count;
+    }
+  };
+  if (Object.prototype.hasOwnProperty.call(aggregation, "histo_agg")) {
+    const buckets = aggregation.histo_agg.buckets;
+    const timestamps = buckets.map((entry: any) => entry.key);
+    const value = buckets.map(extract_value);
+    // we are in the "simple histogram" case
+    return {
+      timestamps,
+      data: [{name: undefined, value }]
+    }
+  } else if (Object.prototype.hasOwnProperty.call(aggregation, "term_agg")) {
+    // we have a term aggregation, but maybe there is an histogram inside
+    const term_buckets = aggregation.term_agg.buckets;
+    if (term_buckets.lenght == 0) {
+      return null;
+    }
+    if (Object.prototype.hasOwnProperty.call(term_buckets[0], "histo_agg")) {
+      // we have a term+histo aggregation
+      const timestamps_set: Set<number> = new Set();
+      term_buckets.forEach((bucket: any) => bucket.histo_agg.buckets.forEach(
+        (entry: any) => timestamps_set.add(entry.key)
+      ));
+      const timestamps = [... timestamps_set];
+      timestamps.sort();
+
+      const data = term_buckets.map((bucket: any) => {
+        const histo_buckets = bucket.histo_agg.buckets;
+        const first_elem_key = histo_buckets[0].key;
+        const last_elem_key = histo_buckets[histo_buckets.length - 1].key;
+        const prefix_len = timestamps.indexOf(first_elem_key);
+        const suffix_len = timestamps.length - timestamps.indexOf(last_elem_key) - 1;
+        const value = Array(prefix_len).fill(0).concat(
+          histo_buckets.map(extract_value),
+          Array(suffix_len).fill(0),
+        );
+
+        return {name: bucket.key, value, }
+      })
+      return {
+        timestamps: timestamps.map((date) => new Date(date)),
+        data,
+      }
+    } else {
+      return term_buckets.map((bucket: any) => {
+        return {
+          term: bucket.key,
+          value: extract_value(bucket),
+        }
+      });
+    }
+  }
+  // we are in neither case??
+  return null;
 }
 
 export const EMPTY_SEARCH_REQUEST: SearchRequest = {
