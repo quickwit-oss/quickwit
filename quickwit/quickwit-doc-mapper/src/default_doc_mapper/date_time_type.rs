0816302fcc90091a508750a9fc322054243878df
trinity-1686a <trinity@quickwit.io>
2024-06-07 16:35:12 +0200

    cast updated fields (#5076)
    * cast stored types if they dont match docmapping when generating hits
    
    this is important when updating the docmapping. Otherwise changing the type of a field makes it disappear from search results for splits using the old mapping

--- quickwit/quickwit-doc-mapper/src/default_doc_mapper/date_time_type.rs
+++ quickwit/quickwit-doc-mapper/src/default_doc_mapper/date_time_type.rs
@@ -18,7 +18,7 @@
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 use indexmap::IndexSet;
-use quickwit_datetime::{DateTimeInputFormat, DateTimeOutputFormat};
+use quickwit_datetime::{DateTimeInputFormat, DateTimeOutputFormat, TantivyDateTime};
 use serde::{Deserialize, Deserializer, Serialize};
 use serde_json::Value as JsonValue;
 use tantivy::schema::{DateTimePrecision, OwnedValue as TantivyValue};
@@ -97,6 +97,29 @@ impl QuickwitDateTimeOptions {
             }
         };
         Ok(TantivyValue::Date(date_time))
+    }
+
+    pub(crate) fn reparse_tantivy_value(
+        &self,
+        tantivy_value: &TantivyValue,
+    ) -> Option<TantivyDateTime> {
+        match tantivy_value {
+            TantivyValue::Date(date) => Some(*date),
+            TantivyValue::Str(date_time_str) => {
+                quickwit_datetime::parse_date_time_str(date_time_str, &self.input_formats.0).ok()
+            }
+            TantivyValue::U64(timestamp_u64) => {
+                let timestamp_i64 = (*timestamp_u64).try_into().ok()?;
+                quickwit_datetime::parse_timestamp_int(timestamp_i64, &self.input_formats.0).ok()
+            }
+            TantivyValue::I64(timestamp_i64) => {
+                quickwit_datetime::parse_timestamp_int(*timestamp_i64, &self.input_formats.0).ok()
+            }
+            TantivyValue::F64(timestamp_f64) => {
+                quickwit_datetime::parse_timestamp_float(*timestamp_f64, &self.input_formats.0).ok()
+            }
+            _ => None,
+        }
     }
 }
 
