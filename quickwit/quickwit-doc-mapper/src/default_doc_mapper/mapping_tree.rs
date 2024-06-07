d2deae6014bba06e83389150bf3c53a037cb5e2e
trinity-1686a <trinity@quickwit.io>
2024-06-07 15:30:56 +0200

    update tantivy

--- quickwit/quickwit-doc-mapper/src/default_doc_mapper/mapping_tree.rs
+++ quickwit/quickwit-doc-mapper/src/default_doc_mapper/mapping_tree.rs
@@ -318,7 +318,7 @@ impl MappingLeaf {
                         .map_err(|err_msg| DocParsingError::ValueError(path.join("."), err_msg))?;
                     for concat_value in concat_values {
                         for field in &self.concatenate {
-                            document.add_field_value(*field, concat_value.clone());
+                            document.add_field_value(*field, &concat_value);
                         }
                     }
                 }
@@ -326,7 +326,7 @@ impl MappingLeaf {
                     .typ
                     .value_from_json(el_json_val)
                     .map_err(|err_msg| DocParsingError::ValueError(path.join("."), err_msg))?;
-                document.add_field_value(self.field, value);
+                document.add_field_value(self.field, &value);
             }
             return Ok(());
         }
@@ -338,7 +338,7 @@ impl MappingLeaf {
                 .map_err(|err_msg| DocParsingError::ValueError(path.join("."), err_msg))?;
             for concat_value in concat_values {
                 for field in &self.concatenate {
-                    document.add_field_value(*field, concat_value.clone());
+                    document.add_field_value(*field, &concat_value);
                 }
             }
         }
@@ -346,7 +346,7 @@ impl MappingLeaf {
             .typ
             .value_from_json(json_val)
             .map_err(|err_msg| DocParsingError::ValueError(path.join("."), err_msg))?;
-        document.add_field_value(self.field, value);
+        document.add_field_value(self.field, &value);
         Ok(())
     }
 
@@ -1293,7 +1293,7 @@ mod tests {
         assert_eq!(document.len(), 3);
         let values: Vec<bool> = document
             .get_all(field)
-            .flat_map(|val| (&val).as_bool())
+            .flat_map(|val| val.as_bool())
             .collect();
         assert_eq!(&values, &[true, false, true])
     }
@@ -1345,7 +1345,7 @@ mod tests {
         assert_eq!(document.len(), 2);
         let values: Vec<i64> = document
             .get_all(field)
-            .flat_map(|val| (&val).as_i64())
+            .flat_map(|val| val.as_i64())
             .collect();
         assert_eq!(&values, &[10i64, 20i64]);
     }
@@ -1558,7 +1558,7 @@ mod tests {
         assert_eq!(document.len(), 2);
         let bytes_vec: Vec<&[u8]> = document
             .get_all(field)
-            .flat_map(|val| (&val).as_bytes())
+            .flat_map(|val| val.as_bytes())
             .collect();
         assert_eq!(
             &bytes_vec[..],
