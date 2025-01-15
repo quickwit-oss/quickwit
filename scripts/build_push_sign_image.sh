6aefa901d7f5eccadab0e47eba17119764beab58
Adrien Guillo <adrien.guillo@datadoghq.com>
2025-01-15 15:28:32 -0500

    Pass build metadata

--- scripts/build_push_sign_image.sh
+++ scripts/build_push_sign_image.sh
@@ -19,11 +19,19 @@ PLATFORM="${PLATFORM:-$DEFAULT_PLATFORM}"
 DOCKER_IMAGE="${REGISTRY}/${REPO}:${TAG}"
 METADATA_FILE=$(mktemp)
 
+# Build args
+QW_COMMIT_DATE=$(TZ=UTC0 git log -1 --format=%cd --date=format-local:%Y-%m-%dT%H:%M:%SZ)
+QW_COMMIT_HASH=$(git rev-parse HEAD)
+QW_COMMIT_TAGS=$(git tag --points-at HEAD | tr '\n' ',')
+
 # Build and push the Docker image
 docker buildx build \
-  --platform "$PLATFORM" \
-  -t "$DOCKER_IMAGE" \
   -f Dockerfile \
+  -t "$DOCKER_IMAGE" \
+  --build-arg QW_COMMIT_DATE="$QW_COMMIT_DATE" \
+  --build-arg QW_COMMIT_HASH="$QW_COMMIT_HASH" \
+  --build-arg QW_COMMIT_TAGS="$QW_COMMIT_TAGS" \
+  --platform "$PLATFORM" \
   --label target="$TARGET_ENV" \
   --metadata-file "$METADATA_FILE" \
   --push .
