20769a71c073b335d819001a87888dde0d7c9675
Remi Dettai <rdettai@gmail.com>
2025-01-27 21:52:23 +0100

    Fix default dynamic mapping config (#5653)

--- quickwit/quickwit-doc-mapper/src/doc_mapper/field_mapping_entry.rs
+++ quickwit/quickwit-doc-mapper/src/doc_mapper/field_mapping_entry.rs
@@ -409,6 +409,14 @@ impl TextIndexingOptions {
             fieldnorms: false,
         }
     }
+
+    fn default_dynamic() -> Self {
+        TextIndexingOptions {
+            tokenizer: QuickwitTextTokenizer::default(),
+            record: IndexRecordOption::Basic,
+            fieldnorms: false,
+        }
+    }
 }
 
 impl Default for TextIndexingOptions {
@@ -611,8 +619,11 @@ impl QuickwitJsonOptions {
     /// Build a default QuickwitJsonOptions for dynamic fields.
     pub fn default_dynamic() -> Self {
         QuickwitJsonOptions {
+            description: None,
+            indexing_options: Some(TextIndexingOptions::default_dynamic()),
+            stored: true,
+            expand_dots: true,
             fast: FastFieldOptions::default_enabled(),
-            ..Default::default()
         }
     }
 }
