1826849e76fdd6fd04107a03141c78d1a0b0f6ff
trinity Pointard <trinity.pointard@datadoghq.com>
2025-02-05 15:16:42 +0100

    add sleep time, fixing another flaky test

--- quickwit/quickwit-indexing/src/actors/merge_executor.rs
+++ quickwit/quickwit-indexing/src/actors/merge_executor.rs
@@ -796,6 +796,7 @@ mod tests {
         delete_task_executor_handle
             .process_pending_and_observe()
             .await;
+        universe.sleep(*quickwit_actors::HEARTBEAT).await;
 
         let packager_msgs: Vec<IndexedSplitBatch> = merge_packager_inbox.drain_for_test_typed();
         if !result_docs.is_empty() {
@@ -852,6 +853,7 @@ mod tests {
                 |split| split.split_state == quickwit_metastore::SplitState::MarkedForDeletion
             ));
         }
+        universe.sleep(*quickwit_actors::HEARTBEAT).await;
         test_sandbox.assert_quit().await;
         universe.assert_quit().await;
         Ok(())
