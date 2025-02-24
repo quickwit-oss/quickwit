2225e01443e86f20bc5a5f51d2acbc62416acfd7
Tony Solomonik <tony.solomonik@gmail.com>
2025-02-24 10:35:13 +0200

    URL decode s3 key in s3:ObjectCreated
    When a file includes `:`, it gets converted to `%3A`, and doing
    `GetObject` fails.

--- quickwit/quickwit-indexing/src/source/queue_sources/message.rs
+++ quickwit/quickwit-indexing/src/source/queue_sources/message.rs
@@ -157,7 +157,9 @@ fn uri_from_s3_notification(message: &[u8], ack_id: &str) -> Result<Uri, PreProc
     let bucket = value["Records"][0]["s3"]["bucket"]["name"]
         .as_str()
         .context("invalid S3 notification: Records[0].s3.bucket.name not found")?;
-    Uri::from_str(&format!("s3://{}/{}", bucket, key)).map_err(|e| e.into())
+    let encoded_key = urlencoding::decode(key)
+        .context("invalid S3 notification: Records[0].s3.object.key could not be url decoded")?;
+    Uri::from_str(&format!("s3://{}/{}", bucket, encoded_key)).map_err(|e| e.into())
 }
 
 /// A message for which we know as much of the global processing status as
