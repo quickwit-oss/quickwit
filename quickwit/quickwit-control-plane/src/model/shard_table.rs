f10654a2f77e5297e31554b2f71b374df547db8b
Remi Dettai <rdettai@gmail.com>
2025-03-20 10:18:31 +0100

    Faster shard scaling (#5679)
    * Increase burst limit
    
    * Add shard scaling factor
    
    * Remove maximum sacle up factor
    
    * Explicit scale up field names and comments
    
    * Refactor scaling arbiter to clarify and test the rules

--- quickwit/quickwit-control-plane/src/model/shard_table.rs
+++ quickwit/quickwit-control-plane/src/model/shard_table.rs
@@ -25,7 +25,7 @@ use quickwit_proto::ingest::{Shard, ShardState};
 use quickwit_proto::types::{IndexUid, NodeId, ShardId, SourceId, SourceUid};
 use tracing::{error, info, warn};
 
-/// Limits the number of shards that can be opened for scaling up a source to 5 per minute.
+/// Limits the number of scale up operations that can happen to a source to 5 per minute.
 const SCALING_UP_RATE_LIMITER_SETTINGS: RateLimiterSettings = RateLimiterSettings {
     burst_limit: 5,
     rate_limit: ConstantRate::new(5, Duration::from_secs(60)),
@@ -41,7 +41,9 @@ const SCALING_DOWN_RATE_LIMITER_SETTINGS: RateLimiterSettings = RateLimiterSetti
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
 pub(crate) enum ScalingMode {
-    Up,
+    /// Scale up by adding this number of shards
+    Up(usize),
+    /// Scale down by removing one shard
     Down,
 }
 
@@ -542,7 +544,7 @@ impl ShardTable {
     ) -> Option<bool> {
         let table_entry = self.table_entries.get_mut(source_uid)?;
         let scaling_rate_limiter = match scaling_mode {
-            ScalingMode::Up => &mut table_entry.scaling_up_rate_limiter,
+            ScalingMode::Up(_) => &mut table_entry.scaling_up_rate_limiter,
             ScalingMode::Down => &mut table_entry.scaling_down_rate_limiter,
         };
         Some(scaling_rate_limiter.acquire(1))
@@ -551,7 +553,7 @@ impl ShardTable {
     pub fn drain_scaling_permits(&mut self, source_uid: &SourceUid, scaling_mode: ScalingMode) {
         if let Some(table_entry) = self.table_entries.get_mut(source_uid) {
             let scaling_rate_limiter = match scaling_mode {
-                ScalingMode::Up => &mut table_entry.scaling_up_rate_limiter,
+                ScalingMode::Up(_) => &mut table_entry.scaling_up_rate_limiter,
                 ScalingMode::Down => &mut table_entry.scaling_down_rate_limiter,
             };
             scaling_rate_limiter.drain();
@@ -561,7 +563,7 @@ impl ShardTable {
     pub fn release_scaling_permits(&mut self, source_uid: &SourceUid, scaling_mode: ScalingMode) {
         if let Some(table_entry) = self.table_entries.get_mut(source_uid) {
             let scaling_rate_limiter = match scaling_mode {
-                ScalingMode::Up => &mut table_entry.scaling_up_rate_limiter,
+                ScalingMode::Up(_) => &mut table_entry.scaling_up_rate_limiter,
                 ScalingMode::Down => &mut table_entry.scaling_down_rate_limiter,
             };
             scaling_rate_limiter.release(1);
@@ -572,7 +574,9 @@ impl ShardTable {
 #[derive(Clone, Copy, Default)]
 pub(crate) struct ShardStats {
     pub num_open_shards: usize,
+    /// Average short-term ingestion rate (MiB/s) per open shard
     pub avg_short_term_ingestion_rate: f32,
+    /// Average long-term ingestion rate (MiB/s) per open shard
     pub avg_long_term_ingestion_rate: f32,
 }
 
@@ -1047,7 +1051,7 @@ mod tests {
             source_id: source_id.clone(),
         };
         assert!(shard_table
-            .acquire_scaling_permits(&source_uid, ScalingMode::Up)
+            .acquire_scaling_permits(&source_uid, ScalingMode::Up(1))
             .is_none());
 
         shard_table.add_source(&index_uid, &source_id);
@@ -1060,7 +1064,7 @@ mod tests {
             .available_permits();
 
         assert!(shard_table
-            .acquire_scaling_permits(&source_uid, ScalingMode::Up)
+            .acquire_scaling_permits(&source_uid, ScalingMode::Up(1))
             .unwrap());
 
         let new_available_permits = shard_table
@@ -1132,10 +1136,10 @@ mod tests {
             .available_permits();
 
         assert!(shard_table
-            .acquire_scaling_permits(&source_uid, ScalingMode::Up)
+            .acquire_scaling_permits(&source_uid, ScalingMode::Up(1))
             .unwrap());
 
-        shard_table.release_scaling_permits(&source_uid, ScalingMode::Up);
+        shard_table.release_scaling_permits(&source_uid, ScalingMode::Up(1));
 
         let new_available_permits = shard_table
             .table_entries
