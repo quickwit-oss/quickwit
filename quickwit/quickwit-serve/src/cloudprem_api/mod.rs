aa9856ec8cc27f9d4529be9699c222efa0961d24
trinity Pointard <trinity.pointard@datadoghq.com>
2025-04-28 16:09:39 +0200

    use allowlist for root_search-allowed indexes

--- quickwit/quickwit-serve/src/cloudprem_api/mod.rs
+++ quickwit/quickwit-serve/src/cloudprem_api/mod.rs
@@ -294,9 +294,11 @@ impl CloudPremService for CloudPremServiceImpl {
     ) -> Result<SearchResponse, CloudPremError> {
         // we don't want to ever access customer data here, that has to go through properly audited
         // channels
-        search_request
-            .index_id_patterns
-            .push(format!("-{CLOUD_PREM_INDEX_ID_PATTERN}"));
+        let safe_pattern_char = |c: char| c.is_ascii_alphanumeric() || c == '-' || c == '.';
+
+        search_request.index_id_patterns.retain(|pattern| {
+            pattern.starts_with("otel-") && pattern.chars().all(safe_pattern_char)
+        });
 
         self.search_service
             .root_search(search_request)
