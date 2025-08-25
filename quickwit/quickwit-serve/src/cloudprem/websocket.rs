c8eef9984618db67f77dd76eb3f59db50c2f86de
trinity Pointard <trinity.pointard@datadoghq.com>
2025-08-25 11:46:33 +0200

    exponential backoff and error handling

--- quickwit/quickwit-serve/src/cloudprem/websocket.rs
+++ quickwit/quickwit-serve/src/cloudprem/websocket.rs
@@ -3,9 +3,13 @@
 //! In it, we create a websocket connection to Datadog's edge, and
 //! wait for pseudo-gRPC calls, we then
 
+use std::time::{Duration, Instant};
+
 use bytes::Bytes;
 use futures::{SinkExt, StreamExt};
+use http::StatusCode;
 use prost::Message as ProstMessage;
+use quickwit_common::retry::RetryParams;
 use quickwit_proto::GrpcServiceError;
 use quickwit_proto::cloudprem::*;
 use quickwit_proto::tonic::Code;
@@ -14,9 +18,12 @@ use tokio::task::JoinSet;
 use tokio_tungstenite::connect_async;
 use tokio_tungstenite::tungstenite::client::IntoClientRequest;
 use tokio_tungstenite::tungstenite::error::{
-    Error as TungsteniteError, ProtocolError as TungsteniteProtocolError,
+    Error as TungsteniteError, ProtocolError as TungsteniteProtocolError, TlsError,
 };
 use tokio_tungstenite::tungstenite::protocol::Message;
+use tracing::{error, info, warn};
+
+const MIN_VALID_CONN: Duration = Duration::from_secs(60);
 
 // TODO we need to add trace id propagation, maybe some form of cancelation too?
 
@@ -82,30 +89,25 @@ async fn handle_single_ws_message_and_reply(
 ) {
     let req = match AnyRequest::decode(buffer) {
         Ok(req) => req,
-        Err(_) => todo!(),
+        Err(e) => {
+            warn!("received undecodable protobuf frame: {e:?}");
+            // we can't even reply with an error for that request: we don't know the req_id
+            return;
+        }
     };
     let response = handle_request(server, req).await;
     let message = Message::binary(response.encode_to_vec());
     let _ = response_channel.send(message).await;
 }
 
-fn transform_err(e: TungsteniteError) -> anyhow::Result<()> {
-    match e {
-        TungsteniteError::ConnectionClosed => Ok(()),
-        TungsteniteError::AlreadyClosed => Ok(()),
-        TungsteniteError::Protocol(TungsteniteProtocolError::ResetWithoutClosingHandshake) => {
-            Ok(())
-        }
-        _ => Err(e.into()),
-    }
-}
+enum Never {}
 
 async fn single_websocket(
     target: &str,
     dd_api_key: &str,
     dd_application_key: &str,
     service: CloudPremServiceClient,
-) -> anyhow::Result<()> {
+) -> Result<Never, TungsteniteError> {
     let mut pending_requests = JoinSet::new();
     let (sender, mut receiver) = channel(5);
 
@@ -134,16 +136,13 @@ async fn single_websocket(
         tokio::select! {
                 reply = receiver.recv() => {
                         if let Some(reply) = reply {
-                            if let Err(e) = ws.send(reply).await {
-                                return transform_err(e);
-                            }
+                            ws.send(reply).await?;
                         }
                 },
                 message = ws.next() => {
                         let message = match message {
-                            Some(Ok(message)) => message,
-                            Some(Err(e)) => return transform_err(e),
-                            None => return Ok(()),
+                            Some(res) => res?,
+                            None => return Err(TungsteniteError::ConnectionClosed), // remote isn't supposed to close the connection
                         };
                         match message {
                                 Message::Binary(buffer) => {
@@ -152,25 +151,56 @@ async fn single_websocket(
                                 Message::Text(payload) => {
                                         unreplied_count = 0;
                                         if payload == "ping" {
-                                            sender.send(Message::Text("pong".into())).await?;
+                                            ws.send(Message::Text("pong".into())).await?;
                                         }
                                 },
-                                Message::Close(_close) => todo!(),
+                                Message::Close(_) => return Err(TungsteniteError::ConnectionClosed),
                                 Message::Ping(payload) => {
-                                    sender.send(Message::Pong(payload)).await?;
+                                    ws.send(Message::Pong(payload)).await?;
                                 },
                                 Message::Pong(_) => (),
-                                _ => tracing::warn!("received unsupported frame"),
+                                _ => warn!("received unsupported frame"),
                         }
                 },
                 _ = interval.tick() => {
                         if unreplied_count > 2 {
-                                anyhow::bail!("other side unresponsive");
+                                warn!("other side unresponsive, retrying connection");
+                                return Err(TungsteniteError::ConnectionClosed)
                         }
                         unreplied_count += 1;
-                        sender.send(Message::Text("ping".into())).await?;
+                        ws.send(Message::Text("ping".into())).await?;
                 }
         }
+    }
+}
+
+// transform errors to success if they are errors we expect to run into in normal condition (such as
+// connection reset) for these non-errors, we don't log anything
+fn report_err(e: &TungsteniteError) -> bool {
+    match e {
+        TungsteniteError::ConnectionClosed => false,
+        TungsteniteError::AlreadyClosed => false,
+        TungsteniteError::Protocol(TungsteniteProtocolError::ResetWithoutClosingHandshake) => false,
+        // native tls doesn't less us match on errors
+        // TODO verify we get the same error on linux as on macos
+        TungsteniteError::Tls(TlsError::Native(e))
+            if e.to_string().contains("connection closed via error") =>
+        {
+            // this is just a "we lost the connection" error, in trials, it happened about once
+            // every day in normal operation
+            false
+        }
+        _ => true,
+    }
+}
+
+fn format_err(err: &TungsteniteError) -> String {
+    let prefix = "error in reverse conn:";
+    match err {
+        TungsteniteError::Http(resp) if resp.status() == StatusCode::FORBIDDEN => {
+            format!("{prefix} invalid authentication parameters")
+        }
+        _ => format!("{prefix} {err:?}"),
     }
 }
 
@@ -180,19 +210,38 @@ pub(crate) async fn maintain_websocket(
     dd_application_key: String,
     service: CloudPremServiceClient,
 ) {
-    // TODO maybe add endpoint as a suffix?
+    let backoff = RetryParams {
+        // if this error comes from an incident at DD, we don't want to hammer the api so much that
+        // it never can recover
+        max_delay: Duration::from_secs(150),
+        // we never stop retrying
+        max_attempts: usize::MAX,
+        ..RetryParams::aggressive()
+    };
+    let url = format!("wss://{target}/api/unstable/cloudprem-connection-gateway/connect");
+
+    let mut retry_count = 0;
+
     loop {
-        // TODO some errors should be fatal (invalid token)
-        tracing::info!("new connection");
-        if let Err(e) = single_websocket(
-            &format!("wss://{target}/api/unstable/cloudprem-connection-gateway/connect"),
-            &dd_api_key,
-            &dd_application_key,
-            service.clone(),
-        )
-        .await
-        {
-            tracing::error!("error in reverse conn: {e:?}")
+        info!("new connection");
+        let before_conn = Instant::now();
+        let Err(err) =
+            single_websocket(&url, &dd_api_key, &dd_application_key, service.clone()).await;
+
+        let msg = format_err(&err);
+        let too_short = before_conn.elapsed() < MIN_VALID_CONN;
+        if too_short {
+            error!("{msg}")
+        } else if report_err(&err) {
+            warn!("{msg}")
+        }
+
+        if too_short {
+            retry_count += 1;
+            tokio::time::sleep(backoff.compute_delay(retry_count)).await;
+        } else {
+            retry_count = 0;
+            // we don't need to backoff, the loop already took enough time in itself
         }
     }
 }
