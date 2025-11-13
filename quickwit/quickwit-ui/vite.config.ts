1c04a5447e3476ef08ae9e299a52094991dc8a5e
Arthur Brongniart <arthur.brongniart@datadoghq.com>
2025-11-13 13:30:49 +0100

    [ui] use vite for build and local dev server

---
+++ quickwit/quickwit-ui/vite.config.ts
@@ -0,0 +1,11 @@
+import { UserConfig } from "vite";
+
+export default {
+  server: {
+    proxy: {
+      "/api": "http://127.0.0.1:7280",
+      "/openapi.json": "http://127.0.0.1:7280",
+    },
+    port: 3000,
+  },
+} satisfies UserConfig;
