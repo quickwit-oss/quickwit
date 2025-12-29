71b1e70c0b37877f9988f8ee501b4d7b9bb23377
fulmicoton <paul.masurel@datadoghq.com>
2025-12-29 11:52:11 +0100

    Removing zookeeper and lighter pulsar.

--- quickwit/quickwit-indexing/src/source/mod.rs
+++ quickwit/quickwit-indexing/src/source/mod.rs
@@ -740,7 +740,11 @@ mod tests {
 
 #[cfg(all(
     test,
-    any(feature = "sqs-localstack-tests", feature = "kafka-broker-tests", feature = "pulsar-broker-tests")
+    any(
+        feature = "sqs-localstack-tests",
+        feature = "kafka-broker-tests",
+        feature = "pulsar-broker-tests"
+    )
 ))]
 mod test_setup_helper {
 
