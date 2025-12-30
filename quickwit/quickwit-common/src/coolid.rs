29a4de9a5f140e26b525d61e183670812f10481c
cong.xie <cong.xie@datadoghq.com>
2025-12-30 12:26:18 -0500

    update lint

--- quickwit/quickwit-common/src/coolid.rs
+++ quickwit/quickwit-common/src/coolid.rs
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use rand::prelude::*;
 use rand::distr::Alphanumeric;
+use rand::prelude::*;
 
 const ADJECTIVES: &[&str] = &[
     "aged",
