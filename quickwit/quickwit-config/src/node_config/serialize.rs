9a67a8119422c60d129c5b4602b033fc71b8d204
Nadav Gov-Ari <nadav.govari@datadoghq.com>
2026-01-21 14:19:16 -0500

    Catch empty AZ env vars

--- quickwit/quickwit-config/src/node_config/serialize.rs
+++ quickwit/quickwit-config/src/node_config/serialize.rs
@@ -224,7 +224,11 @@ impl NodeConfigBuilder {
         env_vars: &HashMap<String, String>,
     ) -> anyhow::Result<NodeConfig> {
         let node_id = self.node_id.resolve(env_vars).map(NodeId::new)?;
-        let availability_zone = self.availability_zone.resolve_optional(env_vars)?;
+        let availability_zone = self
+            .availability_zone
+            .resolve_optional(env_vars)?
+            // if the env var is empty, it'll return Some(""), and we don't want that.
+            .filter(|str| !str.is_empty());
 
         let enabled_services = self
             .enabled_services
