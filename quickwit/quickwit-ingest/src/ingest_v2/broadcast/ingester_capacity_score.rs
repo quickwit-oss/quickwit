f3e82275207a209ef16fba61c5895065a56fa0d6
Nadav Gov-Ari <nadav.govari@datadoghq.com>
2026-02-18 11:39:51 -0500

    make assert on non-tests only

--- quickwit/quickwit-ingest/src/ingest_v2/broadcast/ingester_capacity_score.rs
+++ quickwit/quickwit-ingest/src/ingest_v2/broadcast/ingester_capacity_score.rs
@@ -48,6 +48,7 @@ struct WalMemoryCapacityTimeSeries {
 
 impl WalMemoryCapacityTimeSeries {
     fn new(memory_capacity: ByteSize) -> Self {
+        #[cfg(not(test))]
         assert!(memory_capacity.as_u64() > 0);
         Self {
             memory_capacity,
