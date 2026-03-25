c34966c6abf1ea40282af95a5a767fcd59a0331b
Adrien Guillo <adrien.guillo@datadoghq.com>
2026-03-25 13:44:40 -0400

    Set `searcher.warmup_single_split_initial_allocation` default to 300 MB

--- quickwit/quickwit-config/src/node_config/mod.rs
+++ quickwit/quickwit-config/src/node_config/mod.rs
@@ -521,7 +521,7 @@ impl Default for SearcherConfig {
             request_timeout_secs: Self::default_request_timeout_secs(),
             storage_timeout_policy: None,
             warmup_memory_budget: ByteSize::gb(100),
-            warmup_single_split_initial_allocation: ByteSize::gb(1),
+            warmup_single_split_initial_allocation: ByteSize::mb(300),
             lambda: None,
         }
     }
