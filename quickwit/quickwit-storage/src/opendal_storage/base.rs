b21e9b8fd3f246d3c7803d354ba29ba36fafebb1
trinity.pointard <trinity.pointard@datadoghq.com>
2026-09-07 08:40:18 +0000

    add concurrencty controlto gcs

--- quickwit/quickwit-storage/src/opendal_storage/base.rs
+++ quickwit/quickwit-storage/src/opendal_storage/base.rs
@@ -19,8 +19,9 @@ use std::{fmt, io};
 
 use async_trait::async_trait;
 use futures::AsyncWriteExt as FuturesAsyncWriteExt;
-use opendal::layers::RetryLayer;
+use opendal::layers::{ConcurrentLimitLayer, RetryLayer};
 use opendal::{DeleteInput, IntoDeleteInput, Operator};
+use quickwit_common::get_from_env_cached;
 use quickwit_common::uri::Uri;
 use quickwit_metrics::HistogramTimer;
 use tokio::io::{AsyncRead, AsyncReadExt, AsyncWriteExt as TokioAsyncWriteExt};
@@ -62,7 +63,9 @@ impl OpendalStorage {
         cfg: opendal::services::Gcs,
     ) -> Result<Self, StorageResolverError> {
         opendal::install_default();
-        let op = Operator::new(cfg)?.layer(gcs_retry_layer());
+        let op = Operator::new(cfg)?
+            .layer(gcs_retry_layer())
+            .layer(gcs_concurrent_limit_layer());
         Ok(Self::from_operator(uri, op))
     }
 
@@ -85,7 +88,8 @@ impl OpendalStorage {
     ) -> Result<Self, StorageResolverError> {
         let op = Operator::new(cfg)?
             .with_context(opendal::OperationContext::new().with_http_transport(http_transport))
-            .layer(gcs_retry_layer());
+            .layer(gcs_retry_layer())
+            .layer(gcs_concurrent_limit_layer());
         Ok(Self::from_operator(uri, op))
     }
 
@@ -105,6 +109,13 @@ fn gcs_retry_layer() -> RetryLayer {
         .with_min_delay(Duration::from_millis(250))
         .with_max_delay(Duration::from_secs(20))
         .with_max_times(4)
+}
+
+fn gcs_concurrent_limit_layer() -> ConcurrentLimitLayer {
+    // not S3, but using the same environment variable makes sense
+    let max_concurrency: usize =
+        get_from_env_cached!(usize, "QW_S3_MAX_CONCURRENCY", 10_000, false);
+    ConcurrentLimitLayer::new(max_concurrency)
 }
 
 /// We spotted a ever growing usage of RAM in the opendal GCS implementation.
