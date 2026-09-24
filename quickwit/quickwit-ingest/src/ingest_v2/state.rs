af0591a36e16831af9a2ad9484e12465f511ef84
nadav-govari <nadav.govari@datadoghq.com>
2026-09-24 11:14:12 -0400

    Remove ingest observation stream (#6822)
    * Remove ingest observation stream
    
    * PR comments

--- quickwit/quickwit-ingest/src/ingest_v2/state.rs
+++ quickwit/quickwit-ingest/src/ingest_v2/state.rs
@@ -70,13 +70,8 @@ impl InnerIngesterState {
         *self.status_tx.borrow()
     }
 
-    /// Sets the status and notifies observation streams, even if the status has not changed.
-    fn set_status_and_notify(&self, status: IngesterStatus) {
-        self.status_tx.send(status).expect("channel should be open");
-    }
-
     pub async fn set_status(&mut self, status: IngesterStatus) {
-        self.set_status_and_notify(status);
+        self.status_tx.send_replace(status);
         self.cluster
             .set_self_key_value(INGESTER_STATUS_KEY, status.as_json_str_name())
             .await;
@@ -97,8 +92,6 @@ impl InnerIngesterState {
 
         if is_decommissioned {
             self.set_status(IngesterStatus::Decommissioned).await;
-        } else {
-            self.set_status_and_notify(IngesterStatus::Decommissioning);
         }
     }
 
